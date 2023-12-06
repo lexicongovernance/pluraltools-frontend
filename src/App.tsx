41693f51213b0c80bad9501a5ecbd02f6dfd44a0
camilovegag <camilovegag@unisabana.edu.co>
2023-12-06 15:24:59 -0500

    React Router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Landing from './pages/Landing'
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
-  return <h1>Hello Lexicon</h1>
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  )
 }
 
 export default App
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,9 @@
+function Landing() {
+  return (
+    <>
+      <h1>Landing Page</h1>
+    </>
+  )
+}
+
+export default Landing
