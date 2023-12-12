3cec356e6e83e09d58dec5823cbe8c73d59c6572
Diego Alzate <diego35.da@gmail.com>
2023-12-12 12:42:40 -0500

    fix tsc for build

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Landing from './pages/Landing'
 import { Routes, Route } from 'react-router-dom'
-import PassportPopupRedirect from './pages/popup'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
-      <Route path="/popup" element={<PassportPopupRedirect />} />
     </Routes>
   )
 }
