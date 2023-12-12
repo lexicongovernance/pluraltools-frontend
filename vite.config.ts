262f4f60978ea3b4551e477411515f6b12884cc0
Diego Alzate <diego35.da@gmail.com>
2023-12-12 17:24:55 -0500

    Zupass poc (#4)
    * Zupass test (popup not working)
    
    * fix: zupass popup
    
    * zupass signature
    
    * Fetch nonce & Post pcdStr
    
    * Fix useEffect fetching twice the nonce
    
    * include credentials
    
    * fix cookies
    
    * fix nonce
    
    * add fetch users
    
    * fix tsc for build
    
    * add Popup with Pascal casing
    
    * Add env variables
    
    * Add solution for vercel deploy (test)
    
    * Add response.status checks
    
    ---------
    
    Co-authored-by: camilovegag <camilovegag@unisabana.edu.co>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // This is needed because qr-image relies on it being here
+  // https://github.com/alexeyten/qr-image/issues/13#issuecomment-421289572
+  plugins: [react(), nodePolyfills({
+    include: ["process"],
+    globals: {
+      process: true
+    }
+  })],
 })
