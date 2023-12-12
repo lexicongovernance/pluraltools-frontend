5700991ffb105123a0a90cf92f75c1192cece65f
camilovegag <camilovegag@unisabana.edu.co>
2023-12-12 18:54:54 -0500

    Create Register Page (empty)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Landing from './pages/Landing'
+import Register from './pages/Register'
 import { Routes, Route } from 'react-router-dom'
 import PassportPopupRedirect from './pages/Popup'
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/popup" element={<PassportPopupRedirect />} />
     </Routes>
   )
