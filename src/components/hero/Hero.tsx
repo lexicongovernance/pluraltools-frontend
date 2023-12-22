73d63f860f1f4f80ed0ca34850ff1874cf853546
camilovegag <camilovegag@unisabana.edu.co>
2023-12-22 09:09:02 -0500

    Update landing copy

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -2,20 +2,24 @@ import ZupassLoginButton from '../zupassLoginButton';
 import { Section, Body } from './Hero.styled';
 import { FlexColumn } from '../../layout/Layout.styled';
 
-function Hero() {
+type HeroProps = {
+  data: {
+    title: string;
+    body: string;
+    button: string;
+  };
+};
+
+function Hero({ data }: HeroProps) {
   return (
     <Section>
       <img src="/landing-graphic.png" alt="Graphic" />
       <FlexColumn $gap={'3rem'}>
         <FlexColumn $gap={'2rem'}>
-          <h1>Unlock the Future of MEV Research with Plural MEV Grants</h1>
-          <Body>
-            Shape the future of MEV research. Join Plural MEV, earn a share of 100,000 ARB grants,
-            and be part of a groundbreaking conference. Connect your Zupass wallet and contribute to
-            a decentralized MEV ecosystem.
-          </Body>
+          <h1>{data.title}</h1>
+          <Body>{data.body}</Body>
         </FlexColumn>
-        <ZupassLoginButton>Get started</ZupassLoginButton>
+        <ZupassLoginButton>{data.button}</ZupassLoginButton>
       </FlexColumn>
     </Section>
   );
