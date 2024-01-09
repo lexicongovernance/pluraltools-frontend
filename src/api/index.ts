1ce383905a49000b71834ab84b06748a0f5b8139
Diego Alzate <diego35.da@gmail.com>
2024-01-09 14:59:02 -0500

    fix build

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,6 @@
 export { default as fetchCycles } from './fetchCycles';
 export { default as fetchEvents } from './fetchEvents';
 export { default as fetchNonce } from './fetchNonce';
-export { default as fetchOptions } from './fetchOptions';
 export { default as fetchRegistration } from './fetchRegistration';
 export { default as fetchRegistrationData } from './fetchRegistrationData';
 export { default as fetchRegistrationFields } from './fetchRegistrationFields';
