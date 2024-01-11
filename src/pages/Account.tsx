e4ea9e4b0a299d69d7bedf400e60cf696a61f710
camilovegag <camilovegag@unisabana.edu.co>
2024-01-10 20:22:15 -0500

    Initial account page commit

---
+++ src/pages/Account.tsx
@@ -0,0 +1,11 @@
+import { FlexColumn } from '../layout/Layout.styled';
+
+function Account() {
+  return (
+    <FlexColumn>
+      <h2>Finish your registration</h2>
+    </FlexColumn>
+  );
+}
+
+export default Account;
