35e42fccec386b038ce579bd59307471a9e19d61
camilovegag <camilovegag@unisabana.edu.co>
2024-01-12 15:21:18 -0500

    Fetch user groups & add Toast

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,10 +1,13 @@
 import { useForm } from '@tanstack/react-form';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { zodValidator } from '@tanstack/zod-form-adapter';
+import toast, { Toaster } from 'react-hot-toast';
 import { z } from 'zod';
 import fetchGroups from '../api/fetchGroups';
+import fetchUserGroups from '../api/fetchUserGroups';
 import updateUserData from '../api/updateUserData';
 import Button from '../components/button';
+import Chip from '../components/chip';
 import ErrorText from '../components/form/ErrorText';
 import Input from '../components/form/Input';
 import Label from '../components/form/Label';
@@ -13,28 +16,9 @@ import useUser from '../hooks/useUser';
 import { FlexColumn, FlexRow } from '../layout/Layout.styled';
 
 function Account() {
-  const queryClient = useQueryClient();
   const { user } = useUser();
-
-  const form = useForm({
-    defaultValues: {
-      username: '',
-      email: '',
-      group: '',
-    },
-    onSubmit: ({ value }) => {
-      if (user?.id) {
-        mutateUserData({
-          userId: user?.id,
-          username: value.username,
-          email: value.email,
-          groupIds: [value.group],
-        });
-      }
-    },
-  });
-
-  const { Provider, Field, Subscribe } = form;
+  console.log('user:', user);
+  const queryClient = useQueryClient();
 
   const { data: groups } = useQuery({
     queryKey: ['groups'],
@@ -42,6 +26,15 @@ function Account() {
     staleTime: 10000,
     retry: false,
   });
+
+  const { data: userGroups } = useQuery({
+    queryKey: ['user-groups'],
+    queryFn: () => fetchUserGroups(user?.id || ''),
+    enabled: !!user?.id,
+    staleTime: 10000,
+    retry: false,
+  });
+  console.log('userGroups:', userGroups);
 
   const { mutate: mutateUserData } = useMutation({
     mutationFn: updateUserData,
@@ -51,7 +44,31 @@ function Account() {
         queryClient.invalidateQueries({ queryKey: ['user-groups'] });
       }
     },
+    onError: () => {
+      toast.error('There was an error, please try again.');
+    },
   });
+
+  const form = useForm({
+    defaultValues: {
+      username: user?.username || '',
+      email: user?.email || '',
+      group: (userGroups && userGroups[0]?.id) || '',
+    },
+    onSubmit: ({ value }) => {
+      if (user?.id) {
+        mutateUserData({
+          userId: user?.id,
+          username: value.username,
+          email: value.email,
+          groupIds: [value.group],
+        });
+        toast.success('User data updated!');
+      }
+    },
+  });
+
+  const { Provider, Field, Subscribe } = form;
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -59,90 +76,98 @@ function Account() {
     void form.handleSubmit();
   };
 
+  const userRegistered = userGroups && userGroups?.length > 0;
+
   return (
-    <FlexColumn>
-      <h2>Finish your registration</h2>
-      <Provider>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <FlexColumn>
-            <Field
-              name="username"
-              validatorAdapter={zodValidator}
-              validators={{
-                onChange: z
-                  .string()
-                  .min(3, { message: 'Username must be 3 characters or longer.' }),
-              }}
-              children={(field) => (
-                <FlexColumn $gap="0.5rem">
-                  <Label required>Username</Label>
-                  <Input
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <ErrorText>{field.state.meta.errors}</ErrorText>
-                </FlexColumn>
-              )}
-            />
-            <Field
-              name="email"
-              children={(field) => (
-                <FlexColumn $gap="0.5rem">
-                  <Label>Email</Label>
-                  <Input
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  />
-                  <ErrorText>{field.state.meta.errors}</ErrorText>
-                </FlexColumn>
-              )}
-            />
-            <Field
-              name="group"
-              validatorAdapter={zodValidator}
-              validators={{
-                onChange: z.string().min(1, { message: 'Please select a value.' }),
-              }}
-              children={(field) => (
-                <FlexColumn $gap="0.5rem">
-                  <Label required>Group</Label>
-                  <Select
-                    name={field.name}
-                    value={field.state.value}
-                    onBlur={field.handleBlur}
-                    onChange={(e) => field.handleChange(e.target.value)}
-                  >
-                    <option value="" disabled>
-                      Please choose a group
-                    </option>
-                    {groups?.map((group) => (
-                      <option key={group.id} value={group.id}>
-                        {group.name}
-                      </option>
-                    ))}
-                  </Select>
-                  <ErrorText>{field.state.meta.errors}</ErrorText>
-                </FlexColumn>
-              )}
-            />
-            <FlexRow $alignSelf="flex-end">
-              <Subscribe
-                selector={(state) => [state.canSubmit]}
-                children={([canSubmit]) => (
-                  <Button type="submit" disabled={!canSubmit}>
-                    Submit
-                  </Button>
+    <>
+      <Toaster position="top-center" />
+      <FlexColumn>
+        <FlexRow $justifyContent="space-between">
+          <h2>{userRegistered ? 'User data' : 'Finish your registration'}</h2>
+          {userRegistered && <Chip>Registered</Chip>}
+        </FlexRow>
+        <Provider>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <FlexColumn>
+              <Field
+                name="username"
+                validatorAdapter={zodValidator}
+                validators={{
+                  onChange: z
+                    .string()
+                    .min(3, { message: 'Username must be 3 characters or longer.' }),
+                }}
+                children={(field) => (
+                  <FlexColumn $gap="0.5rem">
+                    <Label required>Username</Label>
+                    <Input
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                    <ErrorText>{field.state.meta.errors}</ErrorText>
+                  </FlexColumn>
                 )}
               />
-            </FlexRow>
-          </FlexColumn>
-        </form>
-      </Provider>
-    </FlexColumn>
+              <Field
+                name="email"
+                children={(field) => (
+                  <FlexColumn $gap="0.5rem">
+                    <Label>Email</Label>
+                    <Input
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    />
+                    <ErrorText>{field.state.meta.errors}</ErrorText>
+                  </FlexColumn>
+                )}
+              />
+              <Field
+                name="group"
+                validatorAdapter={zodValidator}
+                validators={{
+                  onChange: z.string().min(1, { message: 'Please select a value.' }),
+                }}
+                children={(field) => (
+                  <FlexColumn $gap="0.5rem">
+                    <Label required>Group</Label>
+                    <Select
+                      name={field.name}
+                      value={field.state.value}
+                      onBlur={field.handleBlur}
+                      onChange={(e) => field.handleChange(e.target.value)}
+                    >
+                      <option value="" disabled>
+                        Please choose a group
+                      </option>
+                      {groups?.map((group) => (
+                        <option key={group.id} value={group.id}>
+                          {group.name}
+                        </option>
+                      ))}
+                    </Select>
+                    <ErrorText>{field.state.meta.errors}</ErrorText>
+                  </FlexColumn>
+                )}
+              />
+              <FlexRow $alignSelf="flex-end">
+                <Subscribe
+                  selector={(state) => [state.canSubmit]}
+                  children={([canSubmit]) => (
+                    <Button type="submit" disabled={!canSubmit}>
+                      Submit
+                    </Button>
+                  )}
+                />
+              </FlexRow>
+            </FlexColumn>
+          </form>
+        </Provider>
+      </FlexColumn>
+    </>
   );
 }
 
