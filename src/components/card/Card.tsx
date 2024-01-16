0f6e3ad8bb85744352e2f86754263b37f5fe9c76
camilovegag <camilovegag@unisabana.edu.co>
2024-01-15 22:44:01 -0500

    Update Results page with queried statistics

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,15 +1,17 @@
 import { FlexColumn } from '../../layout/Layout.styled';
 import { StyledCard, Title, Body, Icon } from './Card.styled';
 
-type CardProps = { icon: string; title: string; body: string };
+type CardProps = { icon?: string; title: string; body: string | number | undefined };
 
 function Card({ icon, title, body }: CardProps) {
   return (
     <StyledCard>
       <FlexColumn $gap="1.25rem">
-        <Icon>
-          <img src={icon} alt="Icon" />
-        </Icon>
+        {icon && (
+          <Icon>
+            <img src={icon} alt="Icon" />
+          </Icon>
+        )}
         <Title>{title}</Title>
         <Body>{body}</Body>
       </FlexColumn>
--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,7 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { fetchForumQuestionStatistics } from '../api';
 import fetchCycle from '../api/fetchCycle';
+import Card from '../components/card';
+import { FlexColumn, Grid } from '../layout/Layout.styled';
 
 function Results() {
   const { cycleId } = useParams();
@@ -12,6 +15,7 @@ function Results() {
     enabled: !!cycleId,
     retry: false,
   });
+  console.log('cycle:', cycle);
 
   const { data: statistics } = useQuery({
     queryKey: ['cycles', cycleId, 'forumQuestions', 0, 'statistics'],
@@ -20,7 +24,65 @@ function Results() {
     retry: false,
   });
 
-  return <div>{JSON.stringify(statistics)}</div>;
+  const stats = [
+    {
+      id: 0,
+      title: 'Number of proposals:',
+      data: statistics?.numProposals,
+    },
+    {
+      id: 1,
+      title: 'Allocated Hearts:',
+      data: statistics?.sumNumOfHearts,
+    },
+    {
+      id: 2,
+      title: 'Number of participants:',
+      data: statistics?.numOfParticipants,
+    },
+    {
+      id: 3,
+      title: 'Number of groups:',
+      data: statistics?.numOfGroups,
+    },
+  ];
+
+  const optionStatsArray = Object.entries(statistics?.optionStats || {})
+    .map(([id, stats]) => ({
+      id,
+      ...stats,
+    }))
+    .sort((a, b) => b.pluralityScore - a.pluralityScore);
+
+  const ResultsCard = styled.article`
+    background-color: #1f2021;
+    border-radius: 1rem;
+    padding: 2rem;
+  `;
+
+  return (
+    <FlexColumn $gap="4rem">
+      <FlexColumn $gap="3rem">
+        <h2>Results for: {cycle?.forumQuestions?.[0].title}</h2>
+        <Grid $columns={4}>
+          {stats.map((stat) => (
+            <Card key={stat.id} title={stat.title} body={stat.data} />
+          ))}
+        </Grid>
+      </FlexColumn>
+      <FlexColumn $gap="3rem">
+        <h2>Leaderboard</h2>
+        <FlexColumn $gap="2rem">
+          {optionStatsArray.map((option) => (
+            <ResultsCard key={option.id}>
+              <h3>{option.optionTitle}</h3>
+              <p>{option.allocatedHearts}</p>
+            </ResultsCard>
+          ))}
+        </FlexColumn>
+      </FlexColumn>
+    </FlexColumn>
+  );
 }
 
 export default Results;
