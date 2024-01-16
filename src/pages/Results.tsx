b0669f1f08be0e034b0710cf0800bb09bd59dbcb
camilovegag <camilovegag@unisabana.edu.co>
2024-01-16 08:11:30 -0500

    Add results leaderboard

--- src/pages/Results.tsx
+++ src/pages/Results.tsx
@@ -1,12 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { fetchForumQuestionStatistics } from '../api';
 import fetchCycle from '../api/fetchCycle';
 import Card from '../components/card';
-import { FlexColumn, Grid } from '../layout/Layout.styled';
+import { FlexColumn, FlexRow, Grid } from '../layout/Layout.styled';
 
 function Results() {
+  const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
+
   const { cycleId } = useParams();
 
   const { data: cycle } = useQuery({
@@ -53,10 +56,25 @@ function Results() {
     }))
     .sort((a, b) => b.pluralityScore - a.pluralityScore);
 
-  const ResultsCard = styled.article`
+  const ResultsCard = styled.article<{ $expanded: boolean }>`
     background-color: #1f2021;
     border-radius: 1rem;
+    max-height: ${(props) => (props.$expanded ? '100%' : '90px')};
+    overflow: hidden;
     padding: 2rem;
+    transition: max-height 0.3s ease-in-out;
+    cursor: pointer;
+
+    .arrow {
+      transform: rotate(${(props) => (props.$expanded ? '180deg' : '0deg')});
+      transition: transform 0.3s ease-in-out;
+    }
+  `;
+
+  const Badge = styled.div<{ $type: 'gold' | 'silver' | 'bronze' }>`
+    background-image: ${(props) => props.$type && `url('/icons/${props.$type}_badge.svg')`};
+    height: 30px;
+    width: 30px;
   `;
 
   return (
@@ -72,10 +90,28 @@ function Results() {
       <FlexColumn $gap="3rem">
         <h2>Leaderboard</h2>
         <FlexColumn $gap="2rem">
-          {optionStatsArray.map((option) => (
-            <ResultsCard key={option.id}>
-              <h3>{option.optionTitle}</h3>
-              <p>{option.allocatedHearts}</p>
+          {optionStatsArray.map((option, index) => (
+            <ResultsCard
+              key={option.id}
+              $expanded={expandedIndex === index}
+              onClick={() => setExpandedIndex(expandedIndex === index ? null : index)}
+            >
+              <FlexColumn $gap="2rem">
+                <FlexRow $justifyContent="space-between">
+                  <FlexRow $gap="0.5rem">
+                    {index === 0 && <Badge $type="gold" />}
+                    {index === 1 && <Badge $type="silver" />}
+                    {index === 2 && <Badge $type="bronze" />}
+                    <h3>{option.optionTitle}</h3>
+                  </FlexRow>
+                  <img className="arrow" src="/arrow_down.svg" alt="Arrow icon" />
+                </FlexRow>
+                <FlexColumn>
+                  <p>Plurality score: {option.pluralityScore}</p>
+                  <p>Distinct voters: {option.distinctUsers}</p>
+                  <p>Allocated hearts: {option.allocatedHearts}</p>
+                </FlexColumn>
+              </FlexColumn>
             </ResultsCard>
           ))}
         </FlexColumn>
