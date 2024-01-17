b4d789a279897fc90e47eb09042b7f7b87d892d4
camilovegag <camilovegag@unisabana.edu.co>
2024-01-17 10:52:25 -0500

    Update onboarding

--- src/data/register.ts
+++ src/data/register.ts
@@ -2,49 +2,28 @@ const register = {
   onboarding: [
     {
       id: 0,
-      title: `Welcome to Plural MEV!`,
-      body: `Plural MEV is a research community dedicated to fostering collaborative research into MEV, using plural voting and other pluralistic techniques to help reach consensus across boundaries.`,
+      title: `Privacy Ambitions`,
+      body: `A number of our mechanisms (plural voting, peer prediction, your hearts)  require certain social information (e.g., organizational affiliation, academic credentials) to function.`,
     },
     {
       id: 1,
-      title: `❤️ Hearts ❤️`,
-      body: `Community members are given vote power in the form of hearts. Hearts are initially allocated based on expertise, but you can earn more hearts through participation and correctly predicting results.`,
+      title: `ZuPass Registration`,
+      body: `When you register for the community or an in-person event, a PCD (proof-carrying data) will be added to your Zupass which records your attendance and current affiliation.`,
     },
     {
       id: 2,
-      title: `Vote Cycles`,
-      body: `Each vote cycle, allocate your hearts amongst the various options.
-      More hearts means a stronger preference. Once everyone has voted, final scores will be calculated using the connection-oriented cluster match formula.
-      Hearts reset each cycle.`,
+      title: `Incremental Privacy`,
+      body: `We will use these Zupass PCDs to progressively upgrade the forum and voting tool until it works in a purely zero-knowledge fashion. Then, all identifying data will be eliminated from our database. Plurality scores will be calculated with all identities, affiliations and other groupings fully anonymized.`,
     },
     {
       id: 3,
-      title: `Plural Voting`,
-      body: `Plural voting boosts ideas which have support from a broad range of participants.
-      In addition to your vote intensity being square-rooted, a discount is applied to votes from participants with similar profiles.
-      `,
+      title: `Initial Trust Assumptions`,
+      body: `However, since this requires parallel development on Zupass, some of this information will initially be stored in the community’s secure database (managed by Lexicon) , and used solely for the purposes of calculating plurality scores, implementing votes and managing event attendance.`,
     },
     {
       id: 4,
-      title: `Research Grants`,
-      body: `Our first event will be [Berlin Event] on [Date], a pluralistic conference where you, the participants, will set the agenda, content, and ultimately decide who receives a share of 100,000 ARB in MEV research grants.`,
-    },
-    {
-      id: 5,
-      title: `Flash Talks`,
-      body: `Each participant should give a flash talk (5 mins max) outlining a research question in MEV. You’ll be asked to provide a title and brief talk summary.`,
-    },
-    {
-      id: 6,
-      title: `Registration`,
-      body: `On the next page you’ll be asked to provide your sign up information, including your MEV experience and organizational affiliation.
-      This is needed to power the plural voting, but won’t be revealed to any other participants.
-      In future, this will be stored in Zupass, so even we won’t need to access this information.`,
-    },
-    {
-      id: 7,
-      title: `Let’s go!`,
-      body: `Welcome to the future of MEV research. Let’s get started!`,
+      title: `The Future`,
+      body: `With time, we hope that these PCDs can serve as a source of verified affiliations and credentials for gatherings outside of the Plural Research Society.`,
     },
   ],
   form: {
