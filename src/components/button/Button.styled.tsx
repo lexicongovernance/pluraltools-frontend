a5657ed97c91fb2b849173a96831f93dd640958d
camilovegag <camilovegag@unisabana.edu.co>
2024-01-24 10:20:06 -0500

    Update button component

--- src/components/button/Button.styled.tsx
+++ src/components/button/Button.styled.tsx
@@ -4,16 +4,12 @@ import { ButtonProps } from '../../types/ButtonType';
 export const StyledButton = styled.button<ButtonProps>`
   align-self: ${(props) => (props.center ? 'center' : 'flex-start')};
   background-color: var(--color-primary);
-  background-color: ${(props) => props.color === 'secondary' && '#06060C'};
+  background-color: ${(props) => props.color === 'secondary' && 'var(--color-secondary)'};
   background: ${(props) => props.variant === 'text' && 'none'};
   border-radius: 0.5rem;
   border: none;
-  font-size: 1rem;
-  font-weight: 600;
-  line-height: 1.25rem;
   padding-block: 0.5rem;
   padding-inline: 1rem;
-  text-transform: uppercase;
   transition: ease 0.25s;
 
   &:hover {
