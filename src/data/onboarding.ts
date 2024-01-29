2f346431fb853a17954ac8fd5c3577c0082ff011
thewanderingeditor <thewanderingeditor@gmail.com>
2024-01-29 16:16:26 +0000

    Update onboarding.ts
    Updates to onboarding text

--- src/data/onboarding.ts
+++ src/data/onboarding.ts
@@ -2,28 +2,43 @@ const onboarding = {
   data: [
     {
       id: 0,
-      title: `Privacy Ambitions`,
-      body: `A number of our mechanisms (plural voting, peer prediction, your hearts)  require certain social information (e.g., organizational affiliation, academic credentials) to function.`,
+      title: `Ambitions & Trust Assumptions`,
+      body: `Our mechanisms (plural voting, peer prediction) require some social information (e.g., organizational affiliation, academic credentials) to work. Initially, this information will be stored in the community’s secure database (managed by Lexicon), and used solely for the purposes of calculating plurality scores and managing community attendance.`,
     },
     {
       id: 1,
-      title: `ZuPass Registration`,
-      body: `When you register for the community or an in-person event, a PCD (proof-carrying data) will be added to your Zupass which records your attendance and current affiliation.`,
+      title: `ZKDF PCDs`,
+      body: `Our ambition is to add a ZKDF PCD (zero-knowledge data format proof-carrying data) to your Zupass, which will allow us to conduct experiments using zero-knowledge proofs and eliminate this information from our database. We welcome community participation in enhancing privacy.`,
     },
     {
       id: 2,
-      title: `Incremental Privacy`,
-      body: `We will use these Zupass PCDs to progressively upgrade the forum and voting tool until it works in a purely zero-knowledge fashion. Then, all identifying data will be eliminated from our database. Plurality scores will be calculated with all identities, affiliations and other groupings fully anonymized.`,
+      title: `Forkability`,
+      body: `Each member of the Plural Research Society who attends our community gathering will receive a credential represented as a PCD. This will enable anyone to invite members to future convenings with zero-knowledge verification.`,
     },
     {
       id: 3,
-      title: `Initial Trust Assumptions`,
-      body: `However, since this requires parallel development on Zupass, some of this information will initially be stored in the community’s secure database (managed by Lexicon) , and used solely for the purposes of calculating plurality scores, implementing votes and managing event attendance.`,
+      title: `Guiding Principles`,
+      body: `Privacy and Openness: fear of cancellation can stifle discussion. This event is off the record and participants are expected to maintain community privacy.`,
     },
     {
-      id: 4,
-      title: `The Future`,
-      body: `With time, we hope that these PCDs can serve as a source of verified affiliations and credentials for gatherings outside of the Plural Research Society.`,
+      id: 5,
+      title: `Guiding Principles`,
+      body: `Active Participation: Instead of just watching or listening, this event involves discussions, debates, and novel forms of feedback and adaptation.`,
+    },
+    {
+      id: 6,
+      title: `Guiding Principles`,
+      body: `Introvert-Friendly: The environment is designed for introverts, with structured conversations, moderators and plural technology to facilitate ideation and discussions. No need for small talk.`,
+    },
+    {
+      id: 7,
+      title: `Guiding Principles`,
+      body: `Unique and Thought-Provoking: we value novel, unconventional, and intersectional ideas rather than well-known or obvious topics.`,
+    },
+    {
+      id: 7,
+      title: `Guiding Principles`,
+      body: `Selective Attendance: Participants are carefully chosen either through nomination or direct invitation, based on contribution, potential, curiosity, and open-mindedness.`,
     },
   ],
 };
