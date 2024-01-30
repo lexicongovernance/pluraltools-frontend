ae482b11165d75dd6ee82c1718c5bc57c97492b5
Diego Alzate <diego35.da@gmail.com>
2024-01-30 16:59:53 -0500

    Add Subtitle component to Register page (#83)
    * Add Subtitle component to Register page
    
    * Update registration description in RegisterForm

--- src/types/DBEventType.ts
+++ src/types/DBEventType.ts
@@ -2,6 +2,7 @@ export type DBEvent = {
   id: string;
   name: string;
   image_url: string;
+  registrationDescription: string | null;
   createdAt: string;
   updatedAt: string;
   description: string | null;
