0267bde2abcfccab99c7a08a68ef925d82567928
camilovegag <camilovegag@unisabana.edu.co>
2024-01-31 17:14:08 -0500

    Update button

--- src/berlin/components/button/Button.styled.tsx
+++ src/berlin/components/button/Button.styled.tsx
@@ -1,3 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const StyledButton = styled.button``;
+export const StyledButton = styled.button<{ $color: 'primary' | 'secondary' }>`
+  border-radius: 0.5rem;
+  border: none;
+  font-family: var(--font-family-button);
+  font-weight: 600;
+  padding-block: 0.5rem;
+  padding-inline: 1rem;
+  transition: ease 0.25s;
+
+  ${(props) =>
+    props.$color === 'primary' &&
+    css`
+      background-color: var(--color-black);
+      color: var(--color-white);
+    `}
+
+  ${(props) =>
+    props.$color === 'secondary' &&
+    css`
+      background-color: var(--color-white);
+      color: var(--color-black);
+    `}
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
--- src/berlin/components/button/Button.tsx
+++ src/berlin/components/button/Button.tsx
@@ -1,7 +1,17 @@
 import { StyledButton } from './Button.styled';
 
-function Button() {
-  return <StyledButton>Hi</StyledButton>;
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  $color?: 'primary' | 'secondary';
+};
+
+function Button({ children, onClick, $color = 'primary' }: ButtonProps) {
+  return (
+    <StyledButton onClick={onClick} $color={$color}>
+      {children}
+    </StyledButton>
+  );
 }
 
 export default Button;
