a84e8be29f0ac887ced8b7cbfb5dcff4a99a1df4
camilovegag <camilovegag@unisabana.edu.co>
2024-01-31 12:13:22 -0500

    Create new layout

---
+++ src/berlin/layout/Layout.styled.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Main = styled.main`
+  margin-inline: auto;
+  min-height: calc(100vh - 192px);
+  padding-block: 4rem;
+  width: min(85%, 720px);
+`;
