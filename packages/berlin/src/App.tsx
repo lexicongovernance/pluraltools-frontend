4e5cce00120fec3a3b74f31db883bed44cb79bfe
camilovegag <camilovegag@unisabana.edu.co>
2024-02-02 06:19:02 -0500

    Add onboarding page to router

--- packages/berlin/src/App.tsx
+++ packages/berlin/src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from 'react-router-dom';
 
 // Pages
 import Landing from './pages/Landing';
+import Onboarding from './pages/Onboarding';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/onboarding" element={<Onboarding />} />
     </Routes>
   );
 }
