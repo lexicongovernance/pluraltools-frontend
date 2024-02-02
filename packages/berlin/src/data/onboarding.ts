6abe418978c81060878503574c240b62c37143dc
camilovegag <camilovegag@unisabana.edu.co>
2024-02-02 06:23:33 -0500

    Fix data 'Guiding Principles'

--- packages/berlin/src/data/onboarding.ts
+++ packages/berlin/src/data/onboarding.ts
@@ -18,17 +18,9 @@ const onboarding = {
     {
       id: 3,
       title: `Guiding Principles`,
-      body: `Privacy and Openness: fear of cancellation can stifle discussion. This event is off the record and participants are expected to maintain community privacy.`,
-    },
-    {
-      id: 4,
-      title: `Guiding Principles`,
-      body: `Active Participation: Instead of just watching or listening, this event involves discussions, debates, and novel forms of feedback and adaptation.`,
-    },
-    {
-      id: 5,
-      title: `Guiding Principles`,
       body: [
+        `Privacy and Openness: fear of cancellation can stifle discussion. This event is off the record and participants are expected to maintain community privacy.`,
+        `Active Participation: Instead of just watching or listening, this event involves discussions, debates, and novel forms of feedback and adaptation.`,
         `Introvert-Friendly: The environment is designed for introverts, with structured conversations, moderators and plural technology to facilitate ideation and discussions. No need for small talk.`,
         `Unique and Thought-Provoking: we value novel, unconventional, and intersectional ideas rather than well-known or obvious topics.`,
         `Selective Attendance: Participants are carefully chosen either through nomination or direct invitation, based on contribution, potential, curiosity, and open-mindedness.`,
