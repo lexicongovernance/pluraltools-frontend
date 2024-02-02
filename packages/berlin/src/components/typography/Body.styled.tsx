020b1deb915fdde387f228e5ceafa3142111db47
camilovegag <camilovegag@unisabana.edu.co>
2024-02-02 06:16:37 -0500

    Update typography

--- packages/berlin/src/components/typography/Body.styled.tsx
+++ packages/berlin/src/components/typography/Body.styled.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
-export const Body = styled.p`
+type BodyProps = {
+  $minHeight?: string;
+};
+
+export const Body = styled.p<BodyProps>`
   color: var(--color-black);
   font-size: 1.125rem;
   line-height: 1.75rem;
+
+  min-height: ${(props) => props.$minHeight && props.$minHeight};
 `;
