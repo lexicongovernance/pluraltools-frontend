4b757664e519ef2bd5a24a8291879ba596401b49
Diego Alzate <diego35.da@gmail.com>
2024-02-02 10:50:24 -0500

    CORE: update user flow (#102)
    * fix: logout not working as expected
    
    * Add fetchEvents API call and events prop to AccountForm

--- packages/core/src/pages/Account.tsx
+++ packages/core/src/pages/Account.tsx
@@ -18,6 +18,8 @@ import { useAppStore } from '../store';
 import { AuthUser } from '../types/AuthUserType';
 import { GetGroupsResponse } from '../types/GroupType';
 import Select from '../components/select';
+import { fetchEvents } from '../api';
+import { DBEvent } from '../types/DBEventType';
 
 const ACADEMIC_CREDENTIALS = ['Bachelors', 'Masters', 'PhD', 'JD', 'None', 'Other'];
 
@@ -45,6 +47,12 @@ type InitialUser = {
 
 function Account() {
   const { user, isLoading: userIsLoading } = useUser();
+
+  const { data: events } = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+    enabled: !!user?.id,
+  });
 
   const { data: groups } = useQuery({
     queryKey: ['groups'],
@@ -110,7 +118,13 @@ function Account() {
   }
 
   return (
-    <AccountForm initialUser={initialUser} user={user} groups={groups} userGroups={userGroups} />
+    <AccountForm
+      initialUser={initialUser}
+      user={user}
+      groups={groups}
+      userGroups={userGroups}
+      events={events}
+    />
   );
 }
 
@@ -119,11 +133,13 @@ function AccountForm({
   user,
   groups,
   userGroups,
+  events,
 }: {
   initialUser: InitialUser;
   user: AuthUser | null | undefined;
   groups: GetGroupsResponse[] | null | undefined;
   userGroups: GetGroupsResponse[] | null | undefined;
+  events: DBEvent[] | null | undefined;
 }) {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
@@ -199,7 +215,11 @@ function AccountForm({
 
       if (userStatus === 'INCOMPLETE') {
         setUserStatus('COMPLETE');
-        navigate('/events');
+        if (events?.length === 1) {
+          navigate(`/events/${events[0].id}/register`);
+        } else {
+          navigate('/events');
+        }
       }
     }
   };
