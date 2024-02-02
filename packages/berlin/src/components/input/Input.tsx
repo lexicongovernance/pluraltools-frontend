7119fc81bc121175f9e031d76cf17fd4f684153f
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2024-02-02 09:43:05 -0500

    Berlin input component + initial account page (#103)
    * Fix theme
    
    * Create label and error typography
    
    * Create input component
    
    * Create Account page
    
    * Add account page to router
    
    * Update menu icon
    
    * Add 100% witdh to Flex containers
    
    * Set default alignment to flex-start
    
    * Add $justify prop
    
    * Formatting changes
    
    * Add themed arrow icons
    
    * Create select component
    
    * Add account inputs & selects

---
+++ packages/berlin/src/components/input/Input.tsx
@@ -0,0 +1,28 @@
+import { FlexColumn } from '../containers/FlexColum.styled';
+import { Error } from '../typography/Error.styled';
+import Label from '../typography/Label';
+import { StyledInput } from './Input.styled';
+
+type InputProps = {
+  label?: string;
+  required?: boolean;
+  placeholder: string;
+  errors?: string[];
+};
+
+function Input({ label, required, placeholder, errors }: InputProps) {
+  return (
+    <FlexColumn $gap="0.5rem">
+      {label && <Label $required={required}>{label}</Label>}
+      <StyledInput type="text" placeholder={placeholder} />
+      {errors && (
+        <FlexColumn $gap="0.25rem">
+          {errors.map((error) => (
+            <Error>{error}</Error>
+          ))}
+        </FlexColumn>
+      )}
+    </FlexColumn>
+  );
+}
+export default Input;
