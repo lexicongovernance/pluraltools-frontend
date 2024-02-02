853b47ca311e9935b181265b6c4d33458cf24640
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2024-02-02 07:00:42 -0500

    Berlin onboarding (#101)
    * Create dots component
    
    * Add gray to global styles
    
    * Update typography
    
    * Create onboarding page
    
    * Add onboarding page to router
    
    * Fix data 'Guiding Principles'
    
    * Remove cookie setter

---
+++ packages/berlin/src/data/onboarding.ts
@@ -0,0 +1,32 @@
+const onboarding = {
+  data: [
+    {
+      id: 0,
+      title: `Ambitions & Trust Assumptions`,
+      body: `Our mechanisms (plural voting, peer prediction) require some social information (e.g., organizational affiliation, academic credentials) to work. Initially, this information will be stored in the community’s secure database (managed by Lexicon), and used solely for the purposes of calculating plurality scores and managing community attendance.`,
+    },
+    {
+      id: 1,
+      title: `ZKDF PCDs`,
+      body: `Our ambition is to add a ZKDF PCD (zero-knowledge data format proof-carrying data) to your Zupass, which will allow us to conduct experiments using zero-knowledge proofs and eliminate this information from our database. We welcome community participation in enhancing privacy.`,
+    },
+    {
+      id: 2,
+      title: `Forkability`,
+      body: `Each member of the Plural Research Society who attends our community gathering will receive a credential represented as a PCD. This will enable anyone to invite members to future convenings with zero-knowledge verification.`,
+    },
+    {
+      id: 3,
+      title: `Guiding Principles`,
+      body: [
+        `Privacy and Openness: fear of cancellation can stifle discussion. This event is off the record and participants are expected to maintain community privacy.`,
+        `Active Participation: Instead of just watching or listening, this event involves discussions, debates, and novel forms of feedback and adaptation.`,
+        `Introvert-Friendly: The environment is designed for introverts, with structured conversations, moderators and plural technology to facilitate ideation and discussions. No need for small talk.`,
+        `Unique and Thought-Provoking: we value novel, unconventional, and intersectional ideas rather than well-known or obvious topics.`,
+        `Selective Attendance: Participants are carefully chosen either through nomination or direct invitation, based on contribution, potential, curiosity, and open-mindedness.`,
+      ],
+    },
+  ],
+};
+
+export default onboarding;
