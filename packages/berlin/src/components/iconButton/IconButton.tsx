ce69924523278f7e9a39bad72b6e9652dd113d21
camilovegag <camilovegag@unisabana.edu.co>
2024-02-05 21:02:28 -0500

    Update IconButton and pass padding as inline styles

--- packages/berlin/src/components/iconButton/IconButton.tsx
+++ packages/berlin/src/components/iconButton/IconButton.tsx
@@ -1,5 +1,27 @@
-function IconButton() {
-  return;
+import Button from '../button';
+import { Icon, IconContainer } from './IconButton.styled';
+
+type IconButtonProps = {
+  $color: 'primary' | 'secondary';
+  icon: {
+    src: string;
+    alt: string;
+  };
+  onClick: () => void;
+};
+
+function IconButton({ $color, icon, onClick }: IconButtonProps) {
+  return (
+    <Button
+      onClick={onClick}
+      $color={$color}
+      style={{ paddingBlock: '0.25rem', paddingInline: '0.5rem' }}
+    >
+      <IconContainer $height={24} $width={24}>
+        <Icon src={icon.src} alt={icon.alt} height={24} width={24} />
+      </IconContainer>
+    </Button>
+  );
 }
 
 export default IconButton;
