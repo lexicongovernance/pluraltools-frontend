f03e94036b6c8a799b8ba1a97572199038050849
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-02-07 11:34:28 +0100

    reload registration page to ensure data is up to date when re directed

--- packages/berlin/src/pages/Holding.tsx
+++ packages/berlin/src/pages/Holding.tsx
@@ -5,11 +5,14 @@ import { useQuery } from '@tanstack/react-query';
 import { FlexColumn } from '../components/containers/FlexColum.styled';
 import { Body } from '../components/typography/Body.styled';
 import Link from '../components/link';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 // API Calls
 import { fetchEvents } from 'api';
 
 function Holding() {
+  const navigate = useNavigate();
+
   const { data: events, isLoading: eventsLoading } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
@@ -19,6 +22,12 @@ function Holding() {
     return <Body>Loading events...</Body>;
   }
 
+  // Define onClick handler to navigate to registration page and trigger reload
+  const handleRegistrationClick = () => {
+    navigate(`/events/${events?.[0]?.id}/register`, { replace: true }); 
+    window.location.reload();
+  };
+
   return (
     <FlexColumn $gap="2rem">
       <Body>Thank you for submitting!</Body>
@@ -26,7 +35,10 @@ function Holding() {
       <Body>We will inform you of the status by April 15, at the latest.</Body>
       <Body>
         If you need to edit your submission, then{' '}
-        <Link to={`/events/${events?.[0]?.id}/register`}>click here</Link>.
+        <Link to="#" onClick={handleRegistrationClick}>
+          click here
+        </Link>
+        .
       </Body>
     </FlexColumn>
   );
