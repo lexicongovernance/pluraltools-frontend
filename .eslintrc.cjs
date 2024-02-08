18bbda32fbee4f8f31a8f68791b10db79d9edae1
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-02-08 09:24:07 +0100

    warn if there are any types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,8 +14,8 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     '@typescript-eslint/ban-ts-comment': 'warn',
-    '@typescript-eslint/no-var-requires': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@tanstack/query/exhaustive-deps': 'off',
     '@typescript-eslint/no-unused-vars': [
         'warn',
