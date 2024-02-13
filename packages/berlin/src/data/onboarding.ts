9de13486a8d7a44643b971e95517e52a40ef680d
thewanderingeditor <thewanderingeditor@gmail.com>
2024-02-13 17:47:37 +0000

    Update onboarding.ts
    Updates to onboarding slides

--- packages/berlin/src/data/onboarding.ts
+++ packages/berlin/src/data/onboarding.ts
@@ -6,18 +6,18 @@ const onboarding = {
       body: [
         {
           id: 0,
-          text: `Our mechanisms (plural voting, peer prediction) require some social information (e.g., organizational affiliation, academic credentials) to work. Initially, this information will be stored in the community’s secure database (managed by Lexicon), and used solely for the purposes of calculating plurality scores and managing community attendance.`,
+          text: `Our mechanisms (plural voting, peer prediction) require some social information to work. Initially, this information will be stored in the community’s secure database, managed by Lexicon, and used only to calculate plurality scores and manage attendance.`,
         },
         {
           id: 1,
-          text: `Our ambition is to add a ZKDF PCD (zero-knowledge data format proof-carrying data) to your Zupass, which will allow us to conduct experiments using zero-knowledge proofs and eliminate this information from our database. We welcome community participation in enhancing privacy.`,
+          text: `Our ambition is to add a ZKDF PCD (zero-knowledge data format proof-carrying data) to your Zupass, so we can conduct future experiments with zero-knowledge proofs and eliminate this information from our database. We welcome community participation in enhancing privacy.`,
         },
       ],
     },
     {
       id: 1,
       title: `Forkability`,
-      body: `Each member of the Plural Research Society who attends our community gathering will receive a credential represented as a PCD. This will enable anyone to invite members to future convenings with zero-knowledge verification.`,
+      body: `In the future, with ZKDF PCDs, anyone can invite members to alternative convenings with zero-knowledge verification.`,
     },
     {
       id: 2,
@@ -42,11 +42,6 @@ const onboarding = {
           id: 3,
           title: `Unique and Thought-Provoking: `,
           text: `We value novel, unconventional, and intersectional ideas rather than well-known or obvious topics.`,
-        },
-        {
-          id: 4,
-          title: `Selective Attendance: `,
-          text: `Participants are carefully chosen either through nomination or direct invitation, based on contribution, potential, curiosity, and open-mindedness.`,
         },
       ],
     },
