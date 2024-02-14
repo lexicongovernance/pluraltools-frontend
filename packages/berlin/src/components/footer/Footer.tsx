c1a3d58b0b9f20c1bb920281c267979759fe78a2
Diego Alzate <diego35.da@gmail.com>
2024-02-14 15:25:35 -0500

    Remove unused import and update logo images in Footer component

--- packages/berlin/src/components/footer/Footer.tsx
+++ packages/berlin/src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 import { useAppStore } from '../../store';
 import { FlexRow } from '../containers/FlexRow.styled';
-import { Copy, FooterContainer, SyledFooter } from './Footer.styled';
+import { FooterContainer, SyledFooter } from './Footer.styled';
 
 function Header() {
   const theme = useAppStore((state) => state.theme);
@@ -8,10 +8,10 @@ function Header() {
     <SyledFooter>
       <FooterContainer $gap="0.75rem" $align="center">
         <FlexRow $justify="center">
+          <img src={`logos/logo.png`} height={24} width={24} />
           <img src={`logos/arbitrum-${theme}.svg`} height={24} width={24} />
           <img src="logos/plurality.svg" height={24} width={24} />
         </FlexRow>
-        <Copy>Powered by Lexicon</Copy>
       </FooterContainer>
     </SyledFooter>
   );
