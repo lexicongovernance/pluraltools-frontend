564d62ad0754a27a36e30ab6f0ca16e6bfd1d6b1
Diego Alzate <diego35.da@gmail.com>
2024-02-14 15:33:21 -0500

    Add links to footer icons

--- packages/berlin/src/components/footer/Footer.tsx
+++ packages/berlin/src/components/footer/Footer.tsx
@@ -1,5 +1,6 @@
 import { useAppStore } from '../../store';
 import { FlexRow } from '../containers/FlexRow.styled';
+import { Link as RouterLink } from 'react-router-dom';
 import { FooterContainer, SyledFooter } from './Footer.styled';
 
 function Header() {
@@ -8,9 +9,15 @@ function Header() {
     <SyledFooter>
       <FooterContainer $gap="0.75rem" $align="center">
         <FlexRow $justify="center">
-          <img src={`logos/logo.png`} height={24} width={24} />
-          <img src={`logos/arbitrum-${theme}.svg`} height={24} width={24} />
-          <img src="logos/plurality.svg" height={24} width={24} />
+          <RouterLink to={'https://github.com/lexicongovernance'}>
+            <img src={`logos/logo.png`} height={24} width={24} />
+          </RouterLink>
+          <RouterLink to={'https://arbitrum.foundation/grants'}>
+            <img src={`logos/arbitrum-${theme}.svg`} height={24} width={24} />
+          </RouterLink>
+          <RouterLink to={'https://twitter.com/pluralitylab'}>
+            <img src="logos/plurality.svg" height={24} width={24} />
+          </RouterLink>
         </FlexRow>
       </FooterContainer>
     </SyledFooter>
