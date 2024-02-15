5d6e3a554f7b562efb339e18893c96f1e9372d63
camilovegag <camilovegag@unisabana.edu.co>
2024-02-15 15:44:24 -0500

    Create Grid container component

---
+++ packages/berlin/src/components/containers/Grid.styled.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+type GridProps = {
+  $columns?: number;
+  $rows?: number;
+  $gap?: string;
+  $colgap?: string;
+  $rowgap?: string;
+};
+
+export const Grid = styled.section<GridProps>`
+  display: grid;
+  gap: ${(props) => props.$gap || '1rem'};
+  column-gap: ${(props) => props.$colgap || '1rem'};
+  row-gap: ${(props) => props.$rowgap || '1rem'};
+  grid-template-columns: repeat(1, 1fr);
+  grid-template-rows: repeat(auto, 1fr);
+
+  @media (min-width: 640px) {
+    grid-template-columns: ${(props) => props.$columns && `repeat(${props.$columns || 1}, 1fr)`};
+    grid-template-rows: ${(props) => props.$rows && `repeat(${props.$rows || 1}, 1fr)`};
+  }
+`;
