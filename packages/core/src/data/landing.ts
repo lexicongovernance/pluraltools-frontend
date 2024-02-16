5d0a2d7f06be183a808f947538d3b38b574d6f73
thewanderingeditor <thewanderingeditor@gmail.com>
2024-02-16 12:13:11 +0000

    Update landing.ts

--- packages/core/src/data/landing.ts
+++ packages/core/src/data/landing.ts
@@ -1,62 +1,8 @@
 const landing = {
   hero: {
-    title: 'An Experiment in Research Independence and Innovation',
-    body: 'We are a community of open-source researchers committed to solving the hardest problems in decentralization, including the challenge of maintaining research and academic independence in the wake of technological vertical integration and hyper-financialization.',
+    title: 'Put your hearts to the test',
+    body: 'This it the test site for the plural tools from Lexicon, sign in with Zupass to begin',
     button: 'Get Started',
-  },
-  explore: {
-    title: 'Harness the Power of Plurality',
-    cards: [
-      {
-        id: 0,
-        icon: '/icons/research.svg',
-        title: 'Collaborative Research',
-        body: 'Join a diverse community of MEV researchers to foster collaboration and innovation',
-      },
-      {
-        id: 1,
-        icon: '/icons/plurality.svg',
-        title: 'Plural Voting',
-        body: 'Quadratic voting combined with correlation discounting surfaces new and diverse ideas and more efficiently allocates funding',
-      },
-      {
-        id: 2,
-        icon: '/icons/decentralized.svg',
-        title: 'Set the Agenda',
-        body: 'Participate in a groundbreaking MEV conference where participants decide the agenda and content',
-      },
-    ],
-  },
-  benefits: {
-    title: '100,000 ARB in Grants',
-    items: [
-      'Sign up with Zupass to join Plural Research',
-      'Submit an MEV research proposal in the form of a flash talk',
-      'Vote on other submissions and the entire event agenda',
-      'At the event, participants vote to distribute 100,000 ARB in research grants',
-    ],
-  },
-  spark: {
-    title: 'Berlin March 2023',
-    body: 'More details coming soon',
-    button: 'Register with Zupass',
-  },
-  innovation: {
-    title: 'A Plural Approach to Research',
-    body: 'Using plural mechanism design, we experiment with novel ways to surface ideas in the public interest that transcend our biases—protocol, politics, purse— while encouraging collaboration and insights across unlikely intersections where breakthroughs tend to emerge.',
-  },
-  when: {
-    title: 'When?',
-    body: 'Our first convening is May 22 & 23 in Berlin, where the community is tasked to allocate 100,000 ARB in research grants. The first topic broadly is MEV, though we accept other proposals related to decentralization, including privacy, security, identity, censorship resistance, data dignity, and partial common ownership — to name just a few. Surprise us.',
-  },
-  how: {
-    title: 'How?',
-    body: 'Sign up with ZuPass and register by clicking the button below. Early submissions before March 31 will be prioritized.',
-    button: 'Register with Zupass',
-  },
-  what: {
-    title: 'What?',
-    body: 'The experiment will be novel, fun, and weird—like the best of research….and Berlin. Through a series of structured interactions, the community will decide how to prioritize research and allocate funds, relying on structured, high-bandwidth deliberations that surface research public goods, while elevating truth and expertise with peer prediction.  The community is interdisciplinary, drawing on insights from science of science, social science, economics, complexity science, cryptography, and philosophy, among other intersections. The community will test and evolve these mechanisms through multiple cycles of feedback and adaptation.',
   },
 };
 
