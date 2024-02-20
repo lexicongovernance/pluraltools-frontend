08bdbf5ba5248718bd9d14d66a38ea6cf8ae921f
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2024-02-20 14:38:08 -0500

    223 display vote options as single column but add visible plurality score to the right (#230)
    * Comment comments icon
    
    * Change Grid to FlexColumn
    
    * Add arrow down icon
    
    * Add expanded functionallity
    
    * Show plurality score
    
    * Fix IconButton onClick
    
    * Add if check to card onClick & organize imports
    
    * Update onClick event handlers in Button and IconButton components
    
    ---------
    
    Co-authored-by: Diego Alzate <diego35.da@gmail.com>

--- packages/berlin/src/components/optionCard/OptionCard.tsx
+++ packages/berlin/src/components/optionCard/OptionCard.tsx
@@ -1,46 +1,76 @@
-import { useEffect, useState } from 'react';
+// React and third-party libraries
+import { useEffect, useMemo, useState } from 'react';
+
+// Store
+import { useAppStore } from '../../store';
+
+// Components
+import { Body } from '../typography/Body.styled';
 import { FlexColumn } from '../containers/FlexColum.styled';
 import { FlexRow } from '../containers/FlexRow.styled';
-import { Body } from '../typography/Body.styled';
 import { Subtitle } from '../typography/Subtitle.styled';
-import { Card } from './OptionCard.styled';
 import IconButton from '../iconButton';
-import { useAppStore } from '../../store';
+
+// Styled Components
+import { Card } from './OptionCard.styled';
 
 type OptionProps = {
   title: string;
   body?: string;
   avaliableHearts: number;
   numOfVotes: number;
+  pluralityScore: number;
   onVote: () => void;
   onUnvote: () => void;
 };
 
-function OptionCard({ title, body, avaliableHearts, numOfVotes, onVote, onUnvote }: OptionProps) {
+function OptionCard({
+  title,
+  body,
+  pluralityScore,
+  avaliableHearts,
+  numOfVotes,
+  onVote,
+  onUnvote,
+}: OptionProps) {
   const theme = useAppStore((state) => state.theme);
   const [localOptionHearts, setLocalOptionHearts] = useState(numOfVotes);
+  const [expanded, setExpanded] = useState(false);
+
+  const formattedPluralityScore = useMemo(() => {
+    const score = parseFloat(String(pluralityScore));
+    return score % 1 === 0 ? score.toFixed(0) : score.toFixed(3);
+  }, [pluralityScore]);
 
   useEffect(() => {
     setLocalOptionHearts(numOfVotes);
   }, [numOfVotes]);
 
-  const handleVoteClick = () => {
+  const handleVoteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     if (avaliableHearts) {
       setLocalOptionHearts((prevLocalOptionHearts) => prevLocalOptionHearts + 1);
       onVote();
     }
   };
 
-  const handleUnvoteClick = () => {
+  const handleUnvoteClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     setLocalOptionHearts((prevLocalOptionHearts) => Math.max(0, prevLocalOptionHearts - 1));
     onUnvote();
   };
 
+  const handleCardClick = () => {
+    if (body) {
+      setExpanded(!expanded);
+    }
+  };
+
   return (
-    <Card>
-      <FlexColumn $gap="2rem">
+    <Card onClick={handleCardClick}>
+      <FlexColumn>
+        <Body>Plurality Score: {formattedPluralityScore}</Body>
         <Subtitle>{title}</Subtitle>
-        {body && <Body>{body}</Body>}
         <FlexRow $gap="0.25rem" $wrap>
           {localOptionHearts > 0 ? (
             Array.from({ length: localOptionHearts }).map((_, id) => (
@@ -53,28 +83,37 @@ function OptionCard({ title, body, avaliableHearts, numOfVotes, onVote, onUnvote
       </FlexColumn>
       <FlexRow>
         <FlexRow>
+          {/* <IconButton
+            onClick={() => {}}
+            $padding={6}
+            $color="secondary"
+            icon={{ src: `/icons/comments-${theme}.svg`, alt: 'Comments icon' }}
+          /> */}
           <IconButton
-            onClick={handleUnvoteClick}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleUnvoteClick(e)}
             disabled={localOptionHearts === 0}
             $padding={6}
             $color="secondary"
             icon={{ src: `/icons/unvote-${theme}.svg`, alt: 'Unvote icon' }}
           />
           <IconButton
-            onClick={handleVoteClick}
+            onClick={(e: React.MouseEvent<HTMLButtonElement>) => handleVoteClick(e)}
             disabled={avaliableHearts === 0}
             $padding={6}
             $color="primary"
             icon={{ src: `/icons/vote-${theme}.svg`, alt: 'Vote icon' }}
           />
         </FlexRow>
-        <IconButton
-          onClick={() => {}}
-          $padding={6}
-          $color="secondary"
-          icon={{ src: `/icons/comments-${theme}.svg`, alt: 'Comments icon' }}
-        />
+        {body && (
+          <IconButton
+            onClick={() => {}}
+            $color="secondary"
+            $flipVertical={expanded}
+            icon={{ src: `/icons/arrow-down-${theme}.svg`, alt: 'Arrow icon' }}
+          />
+        )}
       </FlexRow>
+      {expanded && body && <Body>{body}</Body>}
     </Card>
   );
 }
