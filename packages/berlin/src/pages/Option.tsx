0f42d70ff223a20026dc9c9b1d9493acdf5fbc88
camilovegag <camilovegag@unisabana.edu.co>
2024-02-21 13:42:06 -0500

    Retrieve hearts at option page

--- packages/berlin/src/pages/Option.tsx
+++ packages/berlin/src/pages/Option.tsx
@@ -5,18 +5,29 @@ import BackButton from '../components/backButton';
 import { FlexColumn } from '../components/containers/FlexColum.styled';
 import { FlexRow } from '../components/containers/FlexRow.styled';
 import { Title } from '../components/typography/Title.styled';
+import { fetchUserVotes } from 'api';
+import useUser from '../hooks/useUser';
 
 function Option() {
-  const { optionId } = useParams();
-  const { data: option } = useQuery({
+  const { user } = useUser();
+  const { cycleId, optionId } = useParams();
+  const { data: option, isLoading } = useQuery({
     queryKey: ['option', optionId],
     queryFn: () => fetchOption(optionId || ''),
     enabled: !!optionId,
   });
+  const { data: userVotes } = useQuery({
+    queryKey: ['votes', cycleId],
+    queryFn: () => fetchUserVotes(cycleId || ''),
+    enabled: !!user?.id && !!cycleId,
+    retry: false,
+  });
 
-  console.log('option:', option);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-  const hearts = 1;
+  const hearts = userVotes?.find((option) => optionId === option.optionId)?.numOfVotes || 0;
 
   return (
     <FlexColumn $gap="2rem">
