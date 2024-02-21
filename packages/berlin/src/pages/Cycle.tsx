cf2a2bfc6757e5642e6da62133f3abe59c309eb8
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-02-21 18:09:38 +0100

    Fix cycle labels logic (#241)
    * fix cycle countdown logic
    
    * edit description
    
    * refactor use counddown to allow for timer until an event opens

--- packages/berlin/src/pages/Cycle.tsx
+++ packages/berlin/src/pages/Cycle.tsx
@@ -57,7 +57,7 @@ function Cycle() {
     }
   }, [cycle]);
 
-  const { formattedTime } = useCountdown(startAt, endAt);
+  const { formattedTime, cycleState } = useCountdown(startAt, endAt);
 
   const updateVotesAndHearts = (votes: ResponseUserVotesType) => {
     const givenVotes = votes
@@ -189,8 +189,10 @@ function Cycle() {
         <Subtitle>Welcome {user?.username}! It's time to give your hearts away...</Subtitle>
         <Title>{currentCycle?.questionTitle}</Title>
         <Body>
-          {formattedTime === 'Cycle has expired'
-            ? 'Vote has expired'
+          {cycleState === 'closed'
+            ? 'Vote has expired.'
+            : cycleState === 'upcoming'
+            ? `Vote opens in: ${formattedTime}`
             : `Vote closes in: ${formattedTime}`}
         </Body>
         <Body>
