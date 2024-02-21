ebd4b3f5a7f5f565f52851242aaf07e7ccc37ca5
camilovegag <camilovegag@unisabana.edu.co>
2024-02-21 10:09:32 -0500

    Add voting cards to event page (missing logic)

--- packages/berlin/src/pages/Event.tsx
+++ packages/berlin/src/pages/Event.tsx
@@ -7,11 +7,9 @@ import { fetchEvent, fetchEventCycles } from 'api';
 
 // Components
 import { FlexColumn } from '../components/containers/FlexColum.styled';
-import { Title } from '../components/typography/Title.styled';
-import CycleCard from '../components/cycleCard';
-import { Grid } from '../components/containers/Grid.styled';
 import BackButton from '../components/backButton';
 import EventCard from '../components/eventCard';
+import VotingCards from '../components/votingCards';
 
 function Event() {
   const { eventId } = useParams();
@@ -30,14 +28,7 @@ function Event() {
     <FlexColumn $gap="2rem">
       <BackButton />
       {event && <EventCard event={event} $direction="row" />}
-      <Title>Votes</Title>
-      {eventCycles && (
-        <Grid>
-          {eventCycles.map((cycle) => (
-            <CycleCard key={cycle.id} cycle={cycle} />
-          ))}
-        </Grid>
-      )}
+      {/* {eventCycles && <VotingCards state="open" cards={eventCycles} />} */}
     </FlexColumn>
   );
 }
