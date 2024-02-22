e2f1ae026b6af434e2b3db0011e0fb27bd327fae
Diego Alzate <diego35.da@gmail.com>
2024-02-22 15:33:45 -0500

    Add white-space property to Body component

--- packages/berlin/src/components/typography/Body.styled.tsx
+++ packages/berlin/src/components/typography/Body.styled.tsx
@@ -8,6 +8,6 @@ export const Body = styled.p<BodyProps>`
   color: var(--color-black);
   font-size: 1.125rem;
   line-height: 1.75rem;
-
+  white-space: pre-wrap;
   min-height: ${(props) => props.$minHeight && props.$minHeight};
 `;
