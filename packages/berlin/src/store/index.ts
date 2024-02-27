70d9fd8df2dc09cdcdffc087668e7b5e519099a6
camilovegag <camilovegag@unisabana.edu.co>
2024-02-27 18:22:24 -0500

    Move available hearts to store

--- packages/berlin/src/store/index.ts
+++ packages/berlin/src/store/index.ts
@@ -7,8 +7,10 @@ interface AppState {
   onboardingStatus: COMPLETION_STATUS;
   userStatus: COMPLETION_STATUS;
   theme: 'light' | 'dark';
+  avaliableHearts: number;
   setUserStatus: (status: COMPLETION_STATUS) => void;
   setOnboardingStatus: (status: COMPLETION_STATUS) => void;
+  setAvaliableHearts: (hearts: number) => void;
   toggleTheme: () => void;
   reset: () => void;
 }
@@ -20,9 +22,11 @@ export const useAppStore = create<AppState>()(
         onboardingStatus: 'INCOMPLETE',
         userStatus: 'INCOMPLETE',
         theme: 'dark', // Default theme is dark
+        avaliableHearts: 20, // Set the initial hearts value
         setUserStatus: (status: COMPLETION_STATUS) => set(() => ({ userStatus: status })),
         setOnboardingStatus: (status: COMPLETION_STATUS) =>
           set(() => ({ onboardingStatus: status })),
+        setAvaliableHearts: (hearts: number) => set(() => ({ avaliableHearts: hearts })),
         toggleTheme: () => set((state) => ({ theme: state.theme === 'light' ? 'dark' : 'light' })),
         reset: () => set(() => ({ userStatus: 'INCOMPLETE' })),
       }),
