c42fa6edec2a8376e5b7516d81a3660a66d3a445
thewanderingeditor <thewanderingeditor@gmail.com>
2024-03-07 12:43:34 +0000

    Update Landing.tsx (#275)
    * Update Landing.tsx
    
    Updates to landing page text
    
    * Update Landing.tsx
    
    * Update Holding.tsx (#276)

--- packages/berlin/src/pages/Landing.tsx
+++ packages/berlin/src/pages/Landing.tsx
@@ -20,27 +20,26 @@ function Landing() {
       </Body>
       <Body>
         Our first convening is <Bold>May 28 in Berlin</Bold>, where the community is tasked to
-        allocate 100,000 ARB in research grants. The first topic broadly is MEV, though we accept
-        other proposals related to decentralization, including identity, security, contextual integrity (“privacy”)
-        collusion resistance, data dignity, and partial common ownership — to name just a few.
-        Surprise us.
+        allocate 100,000 ARB in research grants. The topic of the funding mandate is MEV, though we accept 
+        intersectional proposals that touch on decentralization, including identity, security, contextual 
+        integrity (“privacy”), collusion resistance, data dignity, partial common ownership, and AI — to 
+        name just a few.  Surprise us!
       </Body>
       <Body>
         Sign up with{' '}
         <ZupassButton $variant="link">
           <Bold>Zupass here.</Bold>
         </ZupassButton>{' '}
-        Submissions received before March 31 will get more attention.
+        Submissions received before April 15 will get more attention.
       </Body>
       <Body>
         The experiment will be <Bold>novel, fun, and weird</Bold> — like the best of research… and
-        Berlin. Through a series of structured interactions, the community will decide how to
-        prioritize research and allocate funds, relying on high-bandwidth deliberations
-        that surface research public goods, while elevating expertise through peer
-        prediction. We will test and evolve these mechanisms through multiple cycles
-        of feedback and adaptation. The community is interdisciplinary, drawing on insights from science of science,
-        social science, economics, complexity science, cryptography, and philosophy, among other
-        intersections.
+        Berlin. Through a series of structured interactions, the community will decide how to prioritize 
+        research and allocate funds, relying on collusion-resistant voting (plural voting), high-bandwidth 
+        deliberations, social recombination, and peer prediction. We will test and evolve these mechanisms 
+        through multiple cycles of feedback and adaptation. The community is interdisciplinary, drawing on 
+        insights from science of science, social science, economics, complexity science, cryptography, and 
+        philosophy, among other intersections.
       </Body>
     </FlexColumn>
   );
