d4c45376a26bb2db9da3ca21d90b74c48d93c988
Diego Alzate <diego35.da@gmail.com>
2024-03-08 11:07:14 -0500

    Update CycleTable component in Event page

--- packages/berlin/src/pages/Event.tsx
+++ packages/berlin/src/pages/Event.tsx
@@ -70,11 +70,13 @@ function CycleTable({ cycles, status }: { cycles: GetCycleResponse[]; status: 'o
     <div>
       <Title style={{ textTransform: 'capitalize' }}>{status} Agendas</Title>
       <Table
-        columns={['Agenda', formattedColumnText(), 'Action']}
+        columns={['Agenda', formattedColumnText(), '']}
         rows={cycles.map((cycle) => [
           cycle.forumQuestions?.[0]?.questionTitle,
           formatDate(cycle.endAt),
-          <Button onClick={() => handleClick(cycle.id)}>Go to vote</Button>,
+          <Button onClick={() => handleClick(cycle.id)}>
+            {status === 'open' ? 'Vote' : 'RESULTS'}
+          </Button>,
         ])}
       />
     </div>
