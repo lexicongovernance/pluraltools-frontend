c3e1a361f4c2b070ecc99568cbdedc3cff96247c
thewanderingeditor <thewanderingeditor@gmail.com>
2024-03-12 11:42:21 +0000

    Update Landing.tsx (#301)
    * Update Landing.tsx
    
    * Update Landing.tsx
    
    * Update Landing.tsx
    
    * Update Landing.tsx
    
    * Update Landing.tsx

--- packages/berlin/src/pages/Landing.tsx
+++ packages/berlin/src/pages/Landing.tsx
@@ -14,16 +14,26 @@ function Landing() {
         We are a <Bold>community of open-source researchers</Bold> committed to solving the hardest
         problems in decentralization, including the challenge of maintaining research and academic
         independence in the wake of technological vertical integration and hyper-financialization.
-        Using plural mechanism design, we experiment with novel ways to surface ideas in the public
+        Using <Bold>plural mechanism design</Bold>, we experiment with novel ways to surface ideas in the public
         interest that transcend our biases — protocol, politics, purse — while encouraging
         collaboration and insights across unlikely intersections where breakthroughs tend to emerge.
       </Body>
       <Body>
         Our first convening is <Bold>May 28 in Berlin</Bold>, where the community is tasked to
-        allocate 100,000 ARB in research grants. The topic of the funding mandate is MEV, though we accept 
-        intersectional proposals that touch on decentralization, including identity, security, contextual 
-        integrity (“privacy”), collusion resistance, data dignity, partial common ownership, and AI — to 
-        name just a few.  Surprise us!
+        allocate 100,000 ARB in research grants. 
+      </Body>
+      <Body>
+        The <Bold>funding mandate is MEV</Bold>. We invite research proposals that explore MEV 
+        narrowly and broadly — within and across execution layers (L1), rollups (L2s), oracles, 
+        applications, etc. The mandate also extends to related topics such as chain abstraction, 
+        account abstraction, gas fee optimization, AI agents, ZK, trusted hardware, and topics related
+        to decentralization — including richer notions of identity, contextual integrity (“privacy”), and collusion resistance.
+      </Body>
+      <Body>
+      This is a community event, <Bold>where the research community decides the agenda.</Bold> Through a series 
+      of structured interactions, the community will prioritize research and allocate funds, relying on <Bold>deliberation, 
+      plural voting, and peer prediction</Bold> to surface research in the broader public good. We expect the experiment 
+      to be novel, fun, and weird — like the best of research… and Berlin. Surprise the community with your proposals!
       </Body>
       <Body>
         Sign up with{' '}
@@ -31,15 +41,6 @@ function Landing() {
           <Bold>Zupass here.</Bold>
         </ZupassButton>{' '}
         Submissions received before April 15 will get more attention.
-      </Body>
-      <Body>
-        The experiment will be <Bold>novel, fun, and weird</Bold> — like the best of research… and
-        Berlin. Through a series of structured interactions, the community will decide how to prioritize 
-        research and allocate funds, relying on collusion-resistant voting (plural voting), high-bandwidth 
-        deliberations, social recombination, and peer prediction. We will test and evolve these mechanisms 
-        through multiple cycles of feedback and adaptation. The community is interdisciplinary, drawing on 
-        insights from science of science, social science, economics, complexity science, cryptography, and 
-        philosophy, among other intersections.
       </Body>
     </FlexColumn>
   );
