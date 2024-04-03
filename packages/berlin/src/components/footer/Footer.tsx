21f3c1a8a054e18b7042ccdc5f324bc037239b8c
camilovegag <camilovegag@unisabana.edu.co>
2024-04-03 08:31:05 -0500

    Add data file

--- packages/berlin/src/components/footer/Footer.tsx
+++ packages/berlin/src/components/footer/Footer.tsx
@@ -2,35 +2,27 @@ import { useAppStore } from '../../store';
 import { FlexRow } from '../containers/FlexRow.styled';
 import { Link as RouterLink } from 'react-router-dom';
 import { Copy, FooterContainer, SyledFooter } from './Footer.styled';
+import footerData from '../../data/footer';
 
-function Header() {
+function Footer() {
   const theme = useAppStore((state) => state.theme);
+
   return (
     <SyledFooter>
       <FooterContainer $gap="0.75rem" $align="center">
-        <Copy>A grants program funded by Plurality Labs and the Arbitrum DAO</Copy>
-        <Copy>Co-sponsored by RadicalXChange, MetaGov & De-Sci Foundation</Copy>
-        <Copy>Tooling built by Lexicon Governance</Copy>
+        {footerData.copy.map(({ id, text }) => (
+          <Copy key={id}>{text}</Copy>
+        ))}
         <FlexRow $justify="center">
-          <RouterLink to={'https://arbitrum.foundation/grants'}>
-            <img src={`/logos/arbitrum-${theme}.svg`} height={24} width={24} />
-          </RouterLink>
-          <RouterLink to={'https://www.radicalxchange.org/'}>
-            <img src={`/logos/radicalxchange-${theme}.svg`} height={24} width={24} />
-          </RouterLink>
-          <RouterLink to={'https://metagov.org/'}>
-            <img src={`/logos/metagov-${theme}.svg`} height={24} width={24} />
-          </RouterLink>
-          <RouterLink to={'https://www.descifoundation.org/'}>
-            <img src={`/logos/desci-${theme}.svg`} height={24} width={24} />
-          </RouterLink>
-          <RouterLink to={'https://github.com/lexicongovernance'}>
-            <img src={`/logos/lexicon-${theme}.svg`} height={24} width={24} />
-          </RouterLink>
+          {footerData.logos.map((logo) => (
+            <RouterLink key={logo.src} to={logo.link}>
+              <img src={`/logos/${logo.src}-${theme}.svg`} alt={logo.alt} height={24} width={24} />
+            </RouterLink>
+          ))}
         </FlexRow>
       </FooterContainer>
     </SyledFooter>
   );
 }
 
-export default Header;
+export default Footer;
