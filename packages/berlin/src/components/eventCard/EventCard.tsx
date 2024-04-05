baff913abbd18437e8c3d0eed93445dbe8e28aef
Diego Alzate <diego35.da@gmail.com>
2024-04-05 11:37:36 -0500

    Supprt markdown for description (#380)
    * Add link to event card component
    
    * Update import statement for Link component
    
    * Add react-markdown library and render event description as Markdown

--- packages/berlin/src/components/eventCard/EventCard.tsx
+++ packages/berlin/src/components/eventCard/EventCard.tsx
@@ -3,6 +3,10 @@ import { Subtitle } from '../typography/Subtitle.styled';
 import { Body } from '../typography/Body.styled';
 import { Card, CardContent, ImageContainer } from './EventCard.styled';
 import Button from '../button';
+import Link from '../link';
+
+// Third-party libraries
+import Markdown from 'react-markdown';
 
 type EventCardProps = {
   event: GetEventResponse;
@@ -14,11 +18,20 @@ function EventCard({ event, $direction = 'column', onClick }: EventCardProps) {
   return (
     <Card $direction={$direction}>
       <ImageContainer>
-        <img src={event.imageUrl} alt={event.description || `${event.name} image`} />
+        <img src={event.imageUrl} alt={`${event.name} image`} />
       </ImageContainer>
       <CardContent $gap="1.25rem">
         <Subtitle>{event.name}</Subtitle>
-        {event.description && <Body>{event.description}</Body>}
+        {event.description && (
+          <Markdown
+            components={{
+              a: ({ node, ...props }) => <Link to={props.href ?? ''}>{props.children}</Link>,
+              p: ({ node, ...props }) => <Body>{props.children}</Body>,
+            }}
+          >
+            {event.description}
+          </Markdown>
+        )}
         {onClick && <Button onClick={onClick}>Go</Button>}
       </CardContent>
     </Card>
