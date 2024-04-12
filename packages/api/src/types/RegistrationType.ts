ffdef3b6502d416f8c8beb9b3266700f2edd19bc
camilovegag <camilovegag@unisabana.edu.co>
2024-04-12 10:18:24 -0500

    Update registrations type

--- packages/api/src/types/RegistrationType.ts
+++ packages/api/src/types/RegistrationType.ts
@@ -1,7 +1,11 @@
 export type RegistrationStatus = 'DRAFT' | 'APPROVED' | 'PUBLISHED' | null;
+
 export type GetRegistrationsResponseType = {
-  userId: string;
   id?: string | undefined;
-  eventId?: string | undefined;
   status: RegistrationStatus;
+  userId: string;
+  groupId: string | null;
+  eventId?: string | undefined;
+  createdAt: string;
+  updatedAt: string;
 }[];
