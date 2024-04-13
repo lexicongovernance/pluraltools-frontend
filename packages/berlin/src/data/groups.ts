04c1bb0cd23040c97975ada1258f06a1637b9e50
camilovegag <camilovegag@unisabana.edu.co>
2024-04-13 09:28:28 -0500

    Create groups data file

---
+++ packages/berlin/src/data/groups.ts
@@ -0,0 +1,33 @@
+const groups = {
+  create: {
+    subtitle: 'Create a Research Group',
+    body: [
+      {
+        id: 0,
+        text: 'If you are the Lead Researcher or a Solo Researcher, please create a research group.',
+      },
+      {
+        id: 1,
+        text: 'You will get a code that you can share with your collaborators.',
+      },
+    ],
+    buttonText: 'Create group',
+  },
+  join: {
+    subtitle: 'Join a Research Group',
+    body: [
+      {
+        id: 0,
+        text: ' As a collaborator, you can join a research group. Please ask to your Lead Researcher for the group code.',
+      },
+    ],
+    input: {
+      label: 'Research Group Code',
+      placeholder: 'Enter the secret code here...',
+      requiredMessage: 'Code is required',
+    },
+    buttonText: 'Join group',
+  },
+};
+
+export default groups;
