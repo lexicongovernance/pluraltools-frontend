d0acb4bd3f254a1659108bc7bb1b5b1e1108363f
camilovegag <camilovegag@unisabana.edu.co>
2024-04-16 11:31:06 -0500

    Current combobox implementation

--- packages/berlin/src/pages/PublicGroupRegistration.tsx
+++ packages/berlin/src/pages/PublicGroupRegistration.tsx
@@ -1,3 +1,4 @@
+import { Combobox } from '../../../ui/src';
 import Button from '../components/button';
 import { FlexColumn } from '../components/containers/FlexColum.styled';
 import { Form } from '../components/containers/Form.styled';
@@ -5,12 +6,24 @@ import { Body } from '../components/typography/Body.styled';
 import { Subtitle } from '../components/typography/Subtitle.styled';
 import publicGroups from '../data/publicGroups';
 
+const data = [
+  {
+    value: 'polis',
+    label: 'Polis',
+  },
+  {
+    value: 'test',
+    label: 'Test',
+  },
+];
+
 function PublicGroupRegistration() {
   return (
-    <FlexColumn $gap="2rem">
+    <FlexColumn $gap="1.5rem">
       <Subtitle>{publicGroups.copy.subtitle}</Subtitle>
       <Body>{publicGroups.copy.body}</Body>
       <Form>
+        <Combobox data={data} name="public group" />
         <Button>Join public group</Button>
       </Form>
     </FlexColumn>
