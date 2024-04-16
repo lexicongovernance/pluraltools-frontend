7f397e2ee7fb21650eff7baf8ae9b74187025dd1
camilovegag <camilovegag@unisabana.edu.co>
2024-04-16 14:35:11 -0500

    Adding useForm

--- packages/berlin/src/pages/PublicGroupRegistration.tsx
+++ packages/berlin/src/pages/PublicGroupRegistration.tsx
@@ -1,30 +1,92 @@
-import { Combobox } from '../../../ui/src';
-import Button from '../components/button';
-import { FlexColumn } from '../components/containers/FlexColum.styled';
-import { Form } from '../components/containers/Form.styled';
-import { Body } from '../components/typography/Body.styled';
-import { Subtitle } from '../components/typography/Subtitle.styled';
+// React and third-party libraries
+import { Controller, useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+// API
+import { postUserToGroups } from 'api';
+
+// Data
 import publicGroups from '../data/publicGroups';
 
-const data = [
+// Components
+import { Body } from '../components/typography/Body.styled';
+import { FlexColumn } from '../components/containers/FlexColum.styled';
+import { Form } from '../components/containers/Form.styled';
+import { Subtitle } from '../components/typography/Subtitle.styled';
+import Button from '../components/button';
+import Select from '../components/select';
+
+const groupsData = [
   {
-    value: 'polis',
-    label: 'Polis',
+    id: 'polis',
+    name: 'Polis',
   },
   {
-    value: 'test',
-    label: 'Test',
+    id: 'test',
+    name: 'Test',
   },
 ];
 
 function PublicGroupRegistration() {
+  const queryClient = useQueryClient();
+
+  const {
+    control,
+    getValues,
+    setValue,
+    formState: { errors, isValid },
+    handleSubmit,
+    reset,
+  } = useForm({
+    defaultValues: { group: '' },
+  });
+
+  const { mutate: postUserToGroupsMutation } = useMutation({
+    mutationFn: postUserToGroups,
+    onSuccess: (body) => {
+      if (!body) {
+        return;
+      }
+      queryClient.invalidateQueries({ queryKey: ['user', 'groups'] });
+      toast.success(`Joined ${getValues('group')} group succesfully!`);
+    },
+    onError: () => {
+      toast.error('Something went wrong.');
+    },
+  });
+
+  const onSubmit = () => {
+    if (isValid) {
+      postUserToGroupsMutation({ groupId: 'id' });
+      setValue('group', '');
+      reset();
+    }
+  };
+
   return (
     <FlexColumn $gap="1.5rem">
       <Subtitle>{publicGroups.copy.subtitle}</Subtitle>
       <Body>{publicGroups.copy.body}</Body>
-      <Form>
-        <Combobox data={data} name="public group" />
-        <Button>Join public group</Button>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          name="group"
+          control={control}
+          rules={{ required: 'Public group is required' }}
+          render={({ field }) => (
+            <Select
+              label="Public group"
+              placeholder={field.value ? field.value : 'Select a public group'}
+              options={groupsData}
+              value={field.value}
+              errors={[errors.group?.message ?? '']}
+              onBlur={field.onBlur}
+              onChange={field.onChange}
+              required
+            />
+          )}
+        />
+        <Button type="submit">Join public group</Button>
       </Form>
     </FlexColumn>
   );
