06c79b0a8917edbc462759c7f453d8fb7a76b168
camilovegag <camilovegag@unisabana.edu.co>
2024-04-16 17:16:04 -0500

    Update data file

--- packages/berlin/src/data/publicGroups.ts
+++ packages/berlin/src/data/publicGroups.ts
@@ -1,7 +1,7 @@
 const publicGroups = {
   copy: {
-    subtitle: 'Join a Public Group',
-    body: 'Select a public group from the list and complete the details required to join it.',
+    subtitle: 'Join a group',
+    body: 'Select a group from the list in order to join it.',
   },
 };
 
