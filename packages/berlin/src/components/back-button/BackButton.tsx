a92650119d06aed01d6dc88701a05282a291a8d4
Camilo Vega <59750365+camilovegag@users.noreply.github.com>
2024-05-20 02:58:43 -0500

    517 protect results page (#520)
    * Update back button to recieve a fallback route
    
    * Create a loader to redirect to cycle id if cycle is open (protect /results)
    
    * Add fallback route to cycle back button

--- packages/berlin/src/components/back-button/BackButton.tsx
+++ packages/berlin/src/components/back-button/BackButton.tsx
@@ -2,14 +2,27 @@ import { useNavigate } from 'react-router-dom';
 import { useAppStore } from '../../store';
 import IconButton from '../icon-button';
 
-function BackButton() {
+type BackButtonProps = {
+  fallbackRoute?: string;
+};
+
+function BackButton({ fallbackRoute }: BackButtonProps) {
   const navigate = useNavigate();
   const theme = useAppStore((state) => state.theme);
+
+  const handleBackClick = () => {
+    if (fallbackRoute) {
+      navigate(fallbackRoute);
+    } else {
+      navigate(-1);
+    }
+  };
+
   return (
     <IconButton
-      onClick={() => navigate(-1)}
+      onClick={handleBackClick}
       $color="secondary"
-      icon={{ src: `/icons/arrow-back-${theme}.svg`, alt: 'Trash icon' }}
+      icon={{ src: `/icons/arrow-back-${theme}.svg`, alt: 'Back icon' }}
       $padding={0}
     />
   );
