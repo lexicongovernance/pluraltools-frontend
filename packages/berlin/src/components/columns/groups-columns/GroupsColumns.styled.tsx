3d7fbc1dd08de8fafa8ce4455ff138c3fe8e8723
camilovegag <camilovegag@unisabana.edu.co>
2024-05-20 22:29:23 -0500

    Add members as a new column

--- packages/berlin/src/components/columns/groups-columns/GroupsColumns.styled.tsx
+++ packages/berlin/src/components/columns/groups-columns/GroupsColumns.styled.tsx
@@ -5,12 +5,16 @@ import { Grid } from '../../containers/Grid.styled';
 export const Card = styled(Grid)`
   border-bottom: 2px solid var(--color-black);
   gap: 0;
-  grid-template-columns: repeat(2, 1fr) 82px;
+  grid-template-columns: repeat(3, 1fr) 82px;
   width: 100%;
   padding: 1.5rem;
 `;
 
 export const Group = styled(Body)`
+  font-weight: bold;
+`;
+
+export const Members = styled(Body)`
   font-weight: bold;
 `;
 
