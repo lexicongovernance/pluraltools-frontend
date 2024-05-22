b01bda3408dc2b985218161a7f8ae098d4c2a2c4
Martin Benedikt Busch <bene.busch1992@gmail.com>
2024-05-22 15:40:15 +0200

    fix funding display in results

--- packages/berlin/src/pages/Results.tsx
+++ packages/berlin/src/pages/Results.tsx
@@ -63,7 +63,7 @@ function Results() {
     .map(([id, stats]) => ({
       id,
       ...stats,
-      allocatedFunding: funding?.allocated_funding[id] || null,
+      allocatedFunding: funding?.allocated_funding[id] !== undefined ? funding.allocated_funding[id] : null,
     }))
     .sort((a, b) => parseFloat(b.pluralityScore) - parseFloat(a.pluralityScore));
 
