d5ff37c67d9002cda3552d678d0d46054da972e4
camilovegag <camilovegag@unisabana.edu.co>
2024-05-24 12:15:38 +0100

    Hide spinner at hover

--- packages/berlin/src/components/input/Input.styled.tsx
+++ packages/berlin/src/components/input/Input.styled.tsx
@@ -12,14 +12,17 @@ export const StyledInput = styled.input`
     color: var(--color-gray);
   }
 
-  input::-webkit-outer-spin-button,
-  input::-webkit-inner-spin-button {
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
     -webkit-appearance: none;
     margin: 0;
   }
-
   input[type='number'] {
-    -moz-appearance: textfield; /* Firefox */
-    appearance: textfield; /* Standard property */
+    -moz-appearance: textfield;
+  }
+
+  input[type='number']:hover,
+  input[type='number']:focus {
+    -moz-appearance: textfield;
   }
 `;
