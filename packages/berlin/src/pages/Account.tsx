7ca0c9e7f574b4620b4fe4d991a7c38b20e57d96
Diego Alzate <diego35.da@gmail.com>
2024-06-19 17:14:13 +0100

    fix typo and add lucid react

--- packages/berlin/src/pages/Account.tsx
+++ packages/berlin/src/pages/Account.tsx
@@ -4,8 +4,12 @@ import { Title } from '../components/typography/Title.styled';
 import useUser from '../hooks/useUser';
 import { FlexColumn } from '../components/containers/FlexColumn.styled';
 import { FlexRow } from '../components/containers/FlexRow.styled';
-import Button from '../components/button';
 import { TabManager } from '../components/tab-manager';
+import { Edit, X } from 'lucide-react';
+import { GetUserResponse } from 'api';
+import { Subtitle } from '../components/typography/Subtitle.styled';
+import { Separator } from '../components/separator';
+import { Body } from '../components/typography/Body.styled';
 
 function Account() {
   const { user, isLoading: userIsLoading } = useUser();
@@ -25,25 +29,45 @@ function Account() {
           username: user?.username ?? '',
         }}
         title="Edit Account"
+        afterSubmit={() => {
+          setTab('view');
+        }}
       />
     ),
-    view: <AccountHub />,
+    view: <AccountHub user={user} />,
   };
 
   return (
     <FlexColumn>
-      <FlexRow>
-        <Button onClick={() => setTab('view')}>View</Button>
+      <FlexRow $justify="flex-end">
+        {tab === 'view' ? (
+          <Edit
+            onClick={() => {
+              setTab('edit');
+            }}
+          />
+        ) : (
+          <X
+            onClick={() => {
+              setTab('view');
+            }}
+          />
+        )}
       </FlexRow>
       <TabManager tabs={tabs} tab={tab} fallback={<Title>Tab not found</Title>} />
     </FlexColumn>
   );
 }
 
-function AccountHub() {
+function AccountHub({ user }: { user: GetUserResponse | null | undefined }) {
   return (
     <FlexColumn>
-      <Title>Account Hub</Title>
+      <Subtitle>
+        {user?.firstName} {user?.lastName}
+      </Subtitle>
+      <Body>@{user?.username}</Body>
+      <Body>{user?.email}</Body>
+      <Separator />
     </FlexColumn>
   );
 }
