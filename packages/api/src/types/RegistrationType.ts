e053c4fb3363f199df6a0dc73ffe5d2f58684d68
Diego Alzate <diego35.da@gmail.com>
2024-06-19 21:10:36 +0100

    add new query apis and show list in account hub

--- packages/api/src/types/RegistrationType.ts
+++ packages/api/src/types/RegistrationType.ts
@@ -1,4 +1,4 @@
-export type RegistrationStatus = 'DRAFT' | 'APPROVED' | 'PUBLISHED' | null;
+export type RegistrationStatus = 'DRAFT' | 'APPROVED' | 'REJECTED' | null;
 
 export type GetRegistrationResponseType = {
   id?: string | undefined;
@@ -8,6 +8,16 @@ export type GetRegistrationResponseType = {
   eventId?: string | undefined;
   createdAt: string;
   updatedAt: string;
+  event?: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    link: string | null;
+    registrationDescription: string | null;
+    createdAt: string;
+    updatedAt: string;
+    description: string | null;
+  };
 };
 
 export type GetRegistrationsResponseType = GetRegistrationResponseType[];
--- packages/berlin/src/pages/Account.tsx
+++ packages/berlin/src/pages/Account.tsx
@@ -6,14 +6,24 @@ import { FlexColumn } from '../components/containers/FlexColumn.styled';
 import { FlexRow } from '../components/containers/FlexRow.styled';
 import { TabManager } from '../components/tab-manager';
 import { Edit, X } from 'lucide-react';
-import { GetUserResponse } from 'api';
+import {
+  GetUserResponse,
+  fetchCycle,
+  fetchUserOptions,
+  fetchUserRegistrations,
+  fetchUsersToGroups,
+} from 'api';
 import { Subtitle } from '../components/typography/Subtitle.styled';
 import { Separator } from '../components/separator';
 import { Body } from '../components/typography/Body.styled';
+import { useQueries, useQuery } from '@tanstack/react-query';
+import Link from '../components/link';
+import { Underline } from '../components/typography/Underline.styled';
 
 function Account() {
   const { user, isLoading: userIsLoading } = useUser();
   const [tab, setTab] = useState<'view' | 'edit'>('view');
+
   if (userIsLoading) {
     return <Title>Loading...</Title>;
   }
@@ -60,14 +70,70 @@ function Account() {
 }
 
 function AccountHub({ user }: { user: GetUserResponse | null | undefined }) {
+  const { data: registrations } = useQuery({
+    queryKey: ['users', user?.id, 'registrations'],
+    queryFn: () => fetchUserRegistrations(user?.id ?? ''),
+  });
+
+  const { data: options } = useQuery({
+    queryKey: ['users', user?.id, 'options'],
+    queryFn: () => fetchUserOptions(user?.id ?? ''),
+  });
+
+  const { data: usersToGroups } = useQuery({
+    queryKey: ['users', user?.id, 'groups'],
+    queryFn: () => fetchUsersToGroups(user?.id ?? ''),
+  });
+
+  const cycles = useQueries({
+    queries:
+      // can be improved by filtering repeated cycles
+      options?.map((option) => ({
+        queryKey: ['cycles', option.forumQuestion.cycleId],
+        queryFn: () => fetchCycle(option.forumQuestion.cycleId),
+      })) ?? [],
+  });
+
   return (
     <FlexColumn>
-      <Subtitle>
-        {user?.firstName} {user?.lastName}
-      </Subtitle>
-      <Body>@{user?.username}</Body>
-      <Body>{user?.email}</Body>
+      <div>
+        <Subtitle>
+          {user?.firstName} {user?.lastName}
+        </Subtitle>
+        <Body>@{user?.username}</Body>
+        <Body>{user?.email}</Body>
+      </div>
       <Separator />
+      <div>
+        <Subtitle>Events</Subtitle>
+        <FlexColumn>
+          {registrations
+            ?.filter((registrations) => registrations.status !== 'REJECTED')
+            .map((registration) => (
+              <Link to={`/events/${registration.eventId}/cycles`}>
+                - {registration.event?.name}
+              </Link>
+            ))}
+        </FlexColumn>
+      </div>
+      <div>
+        <Subtitle>Proposals</Subtitle>
+        <FlexColumn>
+          {options?.map((option) => (
+            <Link
+              to={`/events/${cycles.find((c) => c.data?.id === option.forumQuestion.cycleId)?.data?.eventId}/cycles/${option.forumQuestion.cycleId}/options/${option.id}`}
+            >
+              - {option.optionTitle}
+            </Link>
+          ))}
+        </FlexColumn>
+      </div>
+      <div>
+        <Subtitle>Groups</Subtitle>
+        <FlexColumn>
+          {usersToGroups?.map((userToGroup) => <Underline>- {userToGroup.group?.name}</Underline>)}
+        </FlexColumn>
+      </div>
     </FlexColumn>
   );
 }
