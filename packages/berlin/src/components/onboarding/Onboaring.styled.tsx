ee6be9fc6ca6d7e9bbb51c4513a55362feec0e1c
camilovegag <camilovegag@unisabana.edu.co>
2024-06-26 16:20:59 -0500

    Rename component and reduce gap

--- packages/berlin/src/components/onboarding/Onboaring.styled.tsx
+++ packages/berlin/src/components/onboarding/Onboaring.styled.tsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 import { FlexColumn } from '../containers/FlexColumn.styled';
 
-export const Card = styled(FlexColumn)`
+export const OnboardingCard = styled(FlexColumn)`
   padding: 0rem;
   background-color: var(--color-white);
   text-align: left;
+  gap: 0.75rem;
 `;
