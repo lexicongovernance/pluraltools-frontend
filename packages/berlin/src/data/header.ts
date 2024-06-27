c4405b83943fc31b3f269645c92564637e14def4
Diego Alzate <diego35.da@gmail.com>
2024-06-27 11:37:32 +0100

    bring edge esmeralda back to frontend repo (#617)
    * add nav bar changes
    
    * change agenda to vote
    
    * Remove unwanted logos and text
    
    * Update layout with flex instead of min height
    
    ---------
    
    Co-authored-by: Martin Benedikt Busch <bene.busch1992@gmail.com>
    Co-authored-by: Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
    Co-authored-by: camilovegag <camilovegag@unisabana.edu.co>
    Co-authored-by: Camilo Vega <59750365+camilovegag@users.noreply.github.com>

--- packages/berlin/src/data/header.ts
+++ packages/berlin/src/data/header.ts
@@ -3,8 +3,8 @@ const header = {
     src: '/logos/logo.png',
     alt: 'Plural Research Experiment logo, a prism with an incoming light beam and a rainbow',
   },
-  title: 'Plural Research Experiment',
-  subtitle: 'An experiment in collective intelligence and innovation',
+  title: 'Protocol Worlds',
+  subtitle: 'Edge Esmeralda',
 };
 
 export default header;
