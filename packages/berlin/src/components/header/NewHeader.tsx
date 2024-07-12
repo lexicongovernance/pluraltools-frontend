fc3748a602811663f263d930059006d90764bb41
camilovegag <camilovegag@unisabana.edu.co>
2024-07-12 09:46:13 -0500

    Modify component

--- packages/berlin/src/components/header/NewHeader.tsx
+++ packages/berlin/src/components/header/NewHeader.tsx
@@ -1,4 +1,3 @@
-import { cn } from '@/lib/utils';
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -6,55 +5,70 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
 } from '@/_components/ui/navigation-menu';
-import Link from '../link';
-import { forwardRef } from 'react';
+import useUser from '@/hooks/useUser';
 import { useAppStore } from '@/store';
+import { useQuery } from '@tanstack/react-query';
+import { fetchEvents } from 'api';
+import { User } from 'lucide-react';
+import { useMemo } from 'react';
 import { NavLink } from 'react-router-dom';
 import Icon from '../icon';
-import { User } from 'lucide-react';
 import ThemeToggler from '../theme-toggler';
 
-const components = [
-  {
-    title: 'Alert Dialog',
-    href: '/docs/primitives/alert-dialog',
-    description:
-      'A modal dialog that interrupts the user with important content and expects a response.',
-  },
-  {
-    title: 'Hover Card',
-    href: '/docs/primitives/hover-card',
-    description: 'For sighted users to preview content available behind a link.',
-  },
-  {
-    title: 'Progress',
-    href: '/docs/primitives/progress',
-    description:
-      'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-  },
-  {
-    title: 'Scroll-area',
-    href: '/docs/primitives/scroll-area',
-    description: 'Visually or semantically separates content.',
-  },
-  {
-    title: 'Tabs',
-    href: '/docs/primitives/tabs',
-    description:
-      'A set of layered sections of content—known as tab panels—that are displayed one at a time.',
-  },
-  {
-    title: 'Tooltip',
-    href: '/docs/primitives/tooltip',
-    description:
-      'A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.',
-  },
-];
+const HeaderLinks = () => {
+  const { user } = useUser();
+  const { data: events } = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+    enabled: !!user,
+  });
+  const links = useMemo(
+    () => [
+      {
+        title: 'My Proposals',
+        to: `/events/${events?.[0].id}/register`,
+      },
+      {
+        title: 'Agenda',
+        to: `/events/${events?.[0].id}/cycles`,
+      },
+    ],
+    [events],
+  );
+
+  return (
+    <>
+      {links.map((link) => (
+        <NavigationMenuItem key={link.to}>
+          <NavigationMenuLink>
+            <NavLink to={link.to}>{link.title}</NavLink>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      ))}
+    </>
+  );
+};
+
+const UserMenu = () => (
+  <NavigationMenuItem>
+    <NavigationMenuTrigger className="flex">
+      <Icon>
+        <User />
+      </Icon>
+    </NavigationMenuTrigger>
+    <NavigationMenuContent className="bg-primary flex flex-col p-4">
+      <NavigationMenuLink>
+        <NavLink to="/account">Account</NavLink>
+      </NavigationMenuLink>
+      <NavigationMenuLink>Log out</NavigationMenuLink>
+    </NavigationMenuContent>
+  </NavigationMenuItem>
+);
 
 export default function NewHeader() {
   const theme = useAppStore((state) => state.theme);
+
   return (
     <header className="bg-primary border-secondary border-b">
       <section className="mx-auto flex min-h-16 w-[min(90%,1080px)] items-center justify-between">
@@ -64,29 +78,8 @@ export default function NewHeader() {
         </div>
         <NavigationMenu>
           <NavigationMenuList className="gap-2">
-            <NavigationMenuItem>
-              <NavigationMenuLink>
-                <NavLink to="/">My Proposals</NavLink>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuLink>
-                <NavLink to="/">Agenda</NavLink>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger className="flex">
-                <Icon>
-                  <User />
-                </Icon>
-              </NavigationMenuTrigger>
-              <NavigationMenuContent className="bg-primary flex flex-col p-4">
-                <NavigationMenuLink>
-                  <NavLink to="/account">Account</NavLink>
-                </NavigationMenuLink>
-                <NavigationMenuLink>Log out</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
+            <HeaderLinks />
+            <UserMenu />
             <NavigationMenuItem>
               <NavigationMenuLink>
                 <ThemeToggler />
@@ -98,83 +91,3 @@ export default function NewHeader() {
     </header>
   );
 }
-
-export function NavigationMenuDemo() {
-  return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="from-muted/50 to-muted flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">shadcn/ui</div>
-                    <p className="text-muted-foreground text-sm leading-tight">
-                      Beautifully designed components built with Radix UI and Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              {components.map((component) => (
-                <ListItem key={component.title} title={component.title} href={component.href}>
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link to="/docs">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
-}
-
-const ListItem = forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(
-  ({ className, title, children, ...props }, ref) => {
-    return (
-      <li>
-        <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              'hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors',
-              className,
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">{children}</p>
-          </a>
-        </NavigationMenuLink>
-      </li>
-    );
-  },
-);
-ListItem.displayName = 'ListItem';
