46e04a6186b42ff6af3fe0b4df2a27bbb9a605ef
camilovegag <camilovegag@unisabana.edu.co>
2024-07-16 00:23:26 -0500

    Finish new header (pending mobile)

--- packages/berlin/src/components/header/NewHeader.tsx
+++ packages/berlin/src/components/header/NewHeader.tsx
@@ -8,41 +8,74 @@ import {
 } from '@/_components/ui/navigation-menu';
 import useUser from '@/hooks/useUser';
 import { useAppStore } from '@/store';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvents } from 'api';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { fetchAlerts, fetchEvents, fetchUserRegistrations, GetUserResponse, logout } from 'api';
 import { User } from 'lucide-react';
 import { useMemo } from 'react';
 import { NavLink } from 'react-router-dom';
 import Icon from '../icon';
 import ThemeToggler from '../theme-toggler';
+import { useNavigate } from 'react-router-dom';
+import ZupassLoginButton from '../zupass-button';
 
-const HeaderLinks = () => {
-  const { user } = useUser();
+const HeaderLinks = ({ user }: { user: GetUserResponse }) => {
   const { data: events } = useQuery({
     queryKey: ['events'],
     queryFn: fetchEvents,
     enabled: !!user,
   });
-  const links = useMemo(
-    () => [
+
+  const { data: registrationsData } = useQuery({
+    queryKey: [user?.id, 'registrations'],
+    queryFn: () => fetchUserRegistrations(user?.id ?? ''),
+    enabled: !!user,
+  });
+
+  const { data: alerts } = useQuery({
+    queryKey: ['alerts'],
+    queryFn: () => fetchAlerts(),
+    enabled: !!user,
+    refetchInterval: 10000,
+  });
+
+  const links = useMemo(() => {
+    const baseLinks = [
       {
         title: 'My Proposals',
-        to: `/events/${events?.[0].id}/register`,
+        link: events ? `/events/${events?.[0].id}/register` : '',
       },
       {
         title: 'Agenda',
-        to: `/events/${events?.[0].id}/cycles`,
+        link: events ? `/events/${events?.[0].id}/cycles` : '',
       },
-    ],
-    [events],
-  );
+    ];
+
+    if (
+      registrationsData?.some((registration) => registration.status === 'APPROVED') &&
+      alerts &&
+      alerts.length > 0
+    ) {
+      const alertsLinks = alerts.map((alert) => ({
+        title: alert.title,
+        link: alert.link || '',
+      }));
+      return [...baseLinks, ...alertsLinks];
+    }
+
+    return baseLinks;
+  }, [events, registrationsData, alerts]);
 
   return (
     <>
-      {links.map((link) => (
-        <NavigationMenuItem key={link.to}>
-          <NavigationMenuLink>
-            <NavLink to={link.to}>{link.title}</NavLink>
+      {links.map(({ title, link }) => (
+        <NavigationMenuItem key={title}>
+          <NavigationMenuLink asChild>
+            <NavLink
+              to={link}
+              className="border-secondary aria-[current=page]:border-b-2 aria-[current=page]:pb-2"
+            >
+              {title}
+            </NavLink>
           </NavigationMenuLink>
         </NavigationMenuItem>
       ))}
@@ -50,36 +83,65 @@ const HeaderLinks = () => {
   );
 };
 
-const UserMenu = () => (
-  <NavigationMenuItem>
-    <NavigationMenuTrigger className="flex">
-      <Icon>
-        <User />
-      </Icon>
-    </NavigationMenuTrigger>
-    <NavigationMenuContent className="bg-primary flex flex-col p-4">
-      <NavigationMenuLink>
-        <NavLink to="/account">Account</NavLink>
-      </NavigationMenuLink>
-      <NavigationMenuLink>Log out</NavigationMenuLink>
-    </NavigationMenuContent>
-  </NavigationMenuItem>
-);
+const UserMenu = () => {
+  const queryClient = useQueryClient();
+  const resetState = useAppStore((state) => state.reset);
+  const navigate = useNavigate();
+
+  const { mutate: mutateLogout } = useMutation({
+    mutationFn: logout,
+    onSuccess: async () => {
+      resetState();
+      await queryClient.invalidateQueries();
+      await queryClient.removeQueries();
+      navigate('/');
+    },
+  });
+  return (
+    <NavigationMenuItem className="relative">
+      <NavigationMenuTrigger className="flex">
+        <Icon>
+          <User />
+        </Icon>
+      </NavigationMenuTrigger>
+      <NavigationMenuContent className="flex flex-col p-4 gap-4">
+        <NavigationMenuLink asChild>
+          <NavLink
+            to="/account"
+            className="border-secondary aria-[current=page]:border-b-2 aria-[current=page]:pb-2"
+          >
+            Account
+          </NavLink>
+        </NavigationMenuLink>
+        <NavigationMenuLink onClick={() => mutateLogout()} className="cursor-pointer">
+          Log out
+        </NavigationMenuLink>
+      </NavigationMenuContent>
+    </NavigationMenuItem>
+  );
+};
 
 export default function NewHeader() {
   const theme = useAppStore((state) => state.theme);
+  const { user } = useUser();
 
   return (
-    <header className="bg-primary border-secondary border-b">
+    <header className="bg-primary border-secondary border-b text-sm">
       <section className="mx-auto flex min-h-16 w-[min(90%,1080px)] items-center justify-between">
         <div className="flex items-center gap-2">
           <img src={`/logos/lexicon-${theme}.svg`} alt="Lexicon Logo" height={32} width={32} />
           <h1 className="text-2xl font-semibold leading-6">Lexicon</h1>
         </div>
-        <NavigationMenu>
-          <NavigationMenuList className="gap-2">
-            <HeaderLinks />
-            <UserMenu />
+        <NavigationMenu className="font-raleway uppercase">
+          <NavigationMenuList className="gap-3">
+            {user ? (
+              <>
+                <HeaderLinks user={user} />
+                <UserMenu />
+              </>
+            ) : (
+              <ZupassLoginButton>Login with Zupass</ZupassLoginButton>
+            )}
             <NavigationMenuItem>
               <NavigationMenuLink>
                 <ThemeToggler />
