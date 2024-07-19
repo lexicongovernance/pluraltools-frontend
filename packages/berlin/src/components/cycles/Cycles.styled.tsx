3b0d2f761b6c4467b25c7553fbee44d43f51b5cb
camilovegag <camilovegag@unisabana.edu.co>
2024-07-18 23:18:58 -0500

    Remove styled

--- packages/berlin/src/components/cycles/Cycles.styled.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { FlexRowToColumn } from '../containers/FlexRowToColumn.styled';
-
-export const CycleContainer = styled(FlexRowToColumn)`
-  border: 1px solid var(--color-black);
-  padding: 2rem;
-  justify-content: space-between;
-  border-radius: 0.5rem;
-`;
