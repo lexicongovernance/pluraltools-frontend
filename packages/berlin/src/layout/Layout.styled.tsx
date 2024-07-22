cbcd1434645f90b62e82a0c3f08932bcc787ed71
Martin Benedikt Busch <43137759+MartinBenediktBusch@users.noreply.github.com>
2024-07-22 17:38:10 +0200

    Text changes  (#693)
    * preliminary landing page changes
    
    * text change
    
    * add placeholder link
    
    * Update favicon
    
    * Update footer
    
    * Adjust layout to footer changes
    
    * add form link
    
    ---------
    
    Co-authored-by: camilovegag <camilovegag@unisabana.edu.co>

--- packages/berlin/src/layout/Layout.styled.tsx
+++ packages/berlin/src/layout/Layout.styled.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.main`
   margin-inline: auto;
-  min-height: calc(100vh - 21.5rem);
+  min-height: calc(100vh - 17.5rem);
   padding-block: 4rem;
   width: min(90%, 1080px);
 `;
