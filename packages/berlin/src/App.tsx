d8496e6f7660683f073690499d2aadfdb766d849
Diego Alzate <diego35.da@gmail.com>
2024-07-30 12:05:02 +0100

    fix logout error

--- packages/berlin/src/App.tsx
+++ packages/berlin/src/App.tsx
@@ -1,6 +1,6 @@
 // React and third-party libraries
-import { QueryClient } from '@tanstack/react-query';
-import { RouterProvider, createBrowserRouter, redirect } from 'react-router-dom';
+import { CancelledError, QueryClient } from '@tanstack/react-query';
+import { RouterProvider, createBrowserRouter, redirect, useRouteError } from 'react-router-dom';
 
 // API
 import { fetchCycle, fetchEvents, fetchRegistrations, fetchUser } from 'api';
@@ -25,79 +25,103 @@ import SecretGroupRegistration from './pages/SecretGroupRegistration.tsx';
  * Redirects the user to the landing page if they are not logged in
  */
 async function redirectToLandingLoader(queryClient: QueryClient) {
-  const user = await queryClient.fetchQuery({
-    queryKey: ['user'],
-    queryFn: () => fetchUser({ serverUrl: import.meta.env.VITE_SERVER_URL }),
-    staleTime: 10000,
-  });
+  try {
+    const user = await queryClient.fetchQuery({
+      queryKey: ['user'],
+      queryFn: () => fetchUser({ serverUrl: import.meta.env.VITE_SERVER_URL }),
+      staleTime: 10000,
+    });
 
-  if (!user) {
+    if (!user) {
+      return redirect('/');
+    }
+    return null;
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+
     return redirect('/');
   }
-  return null;
 }
 
 /**
  * Redirects the user after successful login
  */
 async function redirectAfterLogin(queryClient: QueryClient) {
-  const user = await queryClient.fetchQuery({
-    queryKey: ['user'],
-    queryFn: () => fetchUser({ serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
-
-  if (!user) {
-    return null;
-  }
-
-  if (!user.username) {
-    return redirect('/account');
-  }
-
-  const events = await queryClient.fetchQuery({
-    queryKey: ['events'],
-    queryFn: () => fetchEvents({ serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
-
-  // if there is only one event, redirect to the cycles page
-  if (events?.length === 1) {
-    const registrations = await queryClient.fetchQuery({
-      queryKey: ['event', events?.[0].id, 'registrations'],
-      queryFn: () =>
-        fetchRegistrations({
-          eventId: events?.[0].id || '',
-          serverUrl: import.meta.env.VITE_SERVER_URL,
-        }),
+  try {
+    const user = await queryClient.fetchQuery({
+      queryKey: ['user'],
+      queryFn: () => fetchUser({ serverUrl: import.meta.env.VITE_SERVER_URL }),
     });
 
-    if (
-      registrations &&
-      registrations.every((registration) => registration.status !== 'APPROVED')
-    ) {
-      return redirect(`/events/${events?.[0].id}/register`);
+    if (!user) {
+      return null;
     }
 
-    return redirect(`/events/${events?.[0].id}/cycles`);
-  }
+    if (!user.username) {
+      return redirect('/account');
+    }
 
-  // if there are multiple events, redirect to the events page
-  return redirect('/events');
+    const events = await queryClient.fetchQuery({
+      queryKey: ['events'],
+      queryFn: () => fetchEvents({ serverUrl: import.meta.env.VITE_SERVER_URL }),
+    });
+
+    // if there is only one event, redirect to the cycles page
+    if (events?.length === 1) {
+      const registrations = await queryClient.fetchQuery({
+        queryKey: ['event', events?.[0].id, 'registrations'],
+        queryFn: () =>
+          fetchRegistrations({
+            eventId: events?.[0].id || '',
+            serverUrl: import.meta.env.VITE_SERVER_URL,
+          }),
+      });
+
+      if (
+        registrations &&
+        registrations.every((registration) => registration.status !== 'APPROVED')
+      ) {
+        return redirect(`/events/${events?.[0].id}/register`);
+      }
+
+      return redirect(`/events/${events?.[0].id}/cycles`);
+    }
+
+    // if there are multiple events, redirect to the events page
+    return redirect('/events');
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+
+    return null;
+  }
 }
 
 /**
  * Redirects the user to the only event if there is only one event
  */
 async function redirectToOnlyOneEventLoader(queryClient: QueryClient) {
-  const events = await queryClient.fetchQuery({
-    queryKey: ['events'],
-    queryFn: () => fetchEvents({ serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
+  try {
+    const events = await queryClient.fetchQuery({
+      queryKey: ['events'],
+      queryFn: () => fetchEvents({ serverUrl: import.meta.env.VITE_SERVER_URL }),
+    });
 
-  if (events?.length === 1) {
-    return redirect(`/events/${events?.[0].id}/cycles`);
+    if (events?.length === 1) {
+      return redirect(`/events/${events?.[0].id}/cycles`);
+    }
+
+    return null;
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+
+    return redirect('/');
   }
-
-  return null;
 }
 
 /**
@@ -105,21 +129,28 @@ async function redirectToOnlyOneEventLoader(queryClient: QueryClient) {
  * Redirects the user to the holding page if they don't have any approved registrations
  */
 async function redirectToEventHoldingOrRegister(queryClient: QueryClient, eventId?: string) {
-  const registrations = await queryClient.fetchQuery({
-    queryKey: ['event', eventId, 'registrations'],
-    queryFn: () =>
-      fetchRegistrations({ eventId: eventId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
+  try {
+    const registrations = await queryClient.fetchQuery({
+      queryKey: ['event', eventId, 'registrations'],
+      queryFn: () =>
+        fetchRegistrations({ eventId: eventId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
+    });
 
-  if (!registrations || !registrations.length) {
-    return redirect(`/events/${eventId}/register`);
+    if (!registrations || !registrations.length) {
+      return redirect(`/events/${eventId}/register`);
+    }
+
+    if (!registrations.some((registration) => registration.status === 'APPROVED')) {
+      return redirect(`/events/${eventId}/holding`);
+    }
+
+    return null;
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+    return redirect('/');
   }
-
-  if (!registrations.some((registration) => registration.status === 'APPROVED')) {
-    return redirect(`/events/${eventId}/holding`);
-  }
-
-  return null;
 }
 
 /**
@@ -130,34 +161,48 @@ async function redirectToCycleResultsLoader(
   eventId?: string,
   cycleId?: string,
 ) {
-  const cycle = await queryClient.fetchQuery({
-    queryKey: ['cycles', cycleId],
-    queryFn: () =>
-      fetchCycle({ cycleId: cycleId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
+  try {
+    const cycle = await queryClient.fetchQuery({
+      queryKey: ['cycles', cycleId],
+      queryFn: () =>
+        fetchCycle({ cycleId: cycleId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
+    });
 
-  if (cycle?.status === 'CLOSED') {
-    return redirect(`/events/${eventId}/cycles/${cycleId}/results`);
+    if (cycle?.status === 'CLOSED') {
+      return redirect(`/events/${eventId}/cycles/${cycleId}/results`);
+    }
+
+    return null;
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+    return redirect('/');
   }
-
-  return null;
 }
 
 /**
  * Redirects the user to the cycle page if the cycle is open
  */
 async function redirectToCycleIfOpen(queryClient: QueryClient, eventId?: string, cycleId?: string) {
-  const cycle = await queryClient.fetchQuery({
-    queryKey: ['cycles', cycleId],
-    queryFn: () =>
-      fetchCycle({ cycleId: cycleId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
-  });
+  try {
+    const cycle = await queryClient.fetchQuery({
+      queryKey: ['cycles', cycleId],
+      queryFn: () =>
+        fetchCycle({ cycleId: cycleId || '', serverUrl: import.meta.env.VITE_SERVER_URL }),
+    });
 
-  if (cycle?.status === 'OPEN') {
-    return redirect(`/events/${eventId}/cycles/${cycleId}`);
+    if (cycle?.status === 'OPEN') {
+      return redirect(`/events/${eventId}/cycles/${cycleId}`);
+    }
+
+    return null;
+  } catch (e) {
+    if (e instanceof CancelledError) {
+      console.log('Tanstack cancelled error:', e);
+    }
+    return redirect('/');
   }
-
-  return null;
 }
 
 const router = (queryClient: QueryClient) =>
@@ -168,6 +213,7 @@ const router = (queryClient: QueryClient) =>
     },
     {
       element: <BerlinLayout />,
+      errorElement: <ErrorBoundary />,
       children: [
         {
           path: '/',
@@ -243,6 +289,13 @@ const router = (queryClient: QueryClient) =>
       ],
     },
   ]);
+
+function ErrorBoundary() {
+  const error = useRouteError();
+  console.log('@ErrorBoundary error', error);
+  // Uncaught ReferenceError: path is not defined
+  return <div>Dang!</div>;
+}
 
 function App({ queryClient }: { queryClient: QueryClient }) {
   return <RouterProvider router={router(queryClient)} />;
