19173ce42ce46814b1bf137a01e7fdc99cbf3410
camilovegag <camilovegag@unisabana.edu.co>
2024-08-05 08:16:21 -0500

    Remove tabs from events page

--- packages/berlin/src/pages/Events.tsx
+++ packages/berlin/src/pages/Events.tsx
@@ -1,5 +1,4 @@
 // React and third-party libraries
-import { useMemo, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 // API
@@ -11,11 +10,9 @@ import useUser from '../hooks/useUser';
 // Components
 import { FlexColumn } from '../components/containers/FlexColumn.styled';
 import { Title } from '../components/typography/Title.styled';
-import * as Tabs from '../components/tabs';
 import EventsCards from '../components/events';
 
 function Events() {
-  const [activeTab, setActiveTab] = useState<string>('upcoming');
   const { user } = useUser();
   const { data: events } = useQuery({
     queryKey: ['events'],
@@ -23,26 +20,13 @@ function Events() {
     enabled: !!user?.id,
   });
 
-  const openEvents = useMemo(() => events?.filter((event) => event.status === 'OPEN'), [events]);
-  const closedEvents = useMemo(
-    () => events?.filter((events) => events.status === 'CLOSED'),
-    [events],
-  );
-
-  const tabNames = ['upcoming', 'past'];
-  const tabs = {
-    upcoming: <EventsCards events={openEvents} errorMessage="No upcoming events..." />,
-    past: <EventsCards events={closedEvents} errorMessage="No past events..." />,
-  };
-
   return (
     <FlexColumn $gap="2rem">
       <section className="flex w-full flex-col justify-between gap-2 md:flex-row md:items-center">
         <Title>Events</Title>
-        <Tabs.TabsHeader className="tabs" tabNames={tabNames} onTabChange={setActiveTab} />
       </section>
       <section className="grid w-full gap-4 md:grid-cols-2">
-        <Tabs.TabsManager tabs={tabs} tab={activeTab} fallback={'Tab not found'} />
+        <EventsCards events={events} errorMessage="No events found..." />
       </section>
     </FlexColumn>
   );
